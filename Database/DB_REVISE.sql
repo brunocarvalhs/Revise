ca90744ff1e701d164063bcc22718e25b7716e0b
Lucas Juan Souza de Oliveira Miranda Moraes <corodolorodo123@outlook.com>
2019-08-22 15:31:08 -0300

    Alteração da estrutura do banco de dados

--- Database/DB_REVISE.sql
+++ Database/DB_REVISE.sql
@@ -2,77 +2,70 @@ create database db_revise;
 use db_revise;
 
 
-CREATE TABLE tb_status ( -- OK
+CREATE TABLE tb_status (
 sg_status char(2)not null,
 nm_status varchar(255),
 CONSTRAINT pk_status
 PRIMARY KEY(sg_status)
 );
 
-CREATE TABLE tb_medida( -- OK
+CREATE TABLE tb_medida(
 sg_medida char(2) not null,
 nm_medida varchar(45),
 CONSTRAINT pk_medida
 PRIMARY KEY(sg_medida)
 );
 
-CREATE TABLE tb_manual( -- ANALISAR
-cd_manual int not null,
-ds_manual TEXT,
-CONSTRAINT pk_manual
-PRIMARY KEY(cd_manual)
-);
-
-CREATE TABLE tb_tipo_plano( -- ANALISAR
+CREATE TABLE tb_tipo_plano(
 cd_tipo_plano int not null,
 nm_tipo_plano varchar(255),
 CONSTRAINT pk_tipo_plano
 PRIMARY KEY(cd_tipo_plano)
 );
 
-CREATE TABLE tb_categoria( -- OK
+CREATE TABLE tb_categoria(
 cd_categoria int not null,
 nm_categoria varchar(255),
 CONSTRAINT pk_categoria
 PRIMARY KEY(cd_categoria)
 );
 
-CREATE TABLE tb_marca(	-- OK
+CREATE TABLE tb_marca(
 cd_marca int not null,
 nm_marca varchar(255),
 CONSTRAINT pk_marca
 PRIMARY KEY (cd_marca)
 );
 
-CREATE TABLE tb_tipo_modelo(	-- OK (ACHO DESNESSESARIO)
+CREATE TABLE tb_tipo_modelo(
 cd_tipo_modelo int not null,
 nm_tipo_modelo varchar(255),
 CONSTRAINT pk_tipo_modelo
 PRIMARY KEY(cd_tipo_modelo)
 );
 
-CREATE TABLE tb_tipo_peca(	-- OK
+CREATE TABLE tb_tipo_peca(
 cd_tipo_peca int not null,
 nm_tipo_peca varchar(255),
 CONSTRAINT pk_tipo_peca
 	PRIMARY KEY(cd_tipo_peca)
 );
 
-CREATE TABLE tb_uf(	-- OK
+CREATE TABLE tb_uf(
 sg_uf char(2) not null,
 nm_uf varchar(255),
 CONSTRAINT pk_uf
 	PRIMARY KEY (sg_uf)
 );
 
-CREATE TABLE tb_tipo_usuario(	-- OK
+CREATE TABLE tb_tipo_usuario(
 cd_tipo_usuario int not null,
 nm_tipo_usuario VARCHAR(255),
 CONSTRAINT pk_tipo_usuario
 	PRIMARY KEY(cd_tipo_usuario)
 );
 
-CREATE TABLE tb_tipo_anuncio(	-- OK
+CREATE TABLE tb_tipo_anuncio(
 cd_tipo_anuncio int not null,
 vl_anuncio decimal(5,2),
 nm_tipo_anuncio varchar(255),
@@ -80,12 +73,14 @@ CONSTRAINT pk_tipo_anuncio
 	PRIMARY KEY(cd_tipo_anuncio)
 );
 
-CREATE TABLE tb_plano(	-- OK
+
+
+CREATE TABLE tb_plano(
 cd_plano int not null,
 nm_plano varchar(255),
 ds_plano TEXT,
 cd_tipo_plano int,
-qt_anuncio int, -- ANALISAR EXISTENCIA
+qt_anuncio int,
 qt_veiculo int,
 CONSTRAINT pk_plano
 	PRIMARY KEY (cd_plano),
@@ -94,7 +89,7 @@ CONSTRAINT fk_tipo_plano
 		REFERENCES tb_tipo_plano(cd_tipo_plano)
 );
 
-CREATE TABLE tb_usuario( -- OK
+CREATE TABLE tb_usuario(
 cd_usuario int not null,
 nm_email varchar(255),
 cd_senha varchar(255),
@@ -110,10 +105,10 @@ CONSTRAINT fk_usuario_tipo
 		REFERENCES tb_tipo_usuario(cd_tipo_usuario)
 );
 
-create table tb_usuario_fisico(  -- OK
+create table tb_usuario_fisico(
 cd_usuario_fisico int not null,
 nm_usuario_fisico varchar(255),
-cd_cpf varchar(11) not null,
+cd_cpf int not null,
 cd_usuario int,
 CONSTRAINT pk_usuario_fisico
 	PRIMARY KEY(cd_usuario_fisico),
@@ -122,11 +117,11 @@ CONSTRAINT fk_fisico_usuario
 		REFERENCES tb_usuario(cd_usuario)
 );
 
-create table tb_usuario_juridico(	-- OK
+create table tb_usuario_juridico(
 cd_usuario_juridico int not null,
 nm_nome_fantasia varchar(255),
 nm_razao_social VARCHAR(255),
-cd_cnpj varchar(14) not null,
+cd_cnpj int,
 cd_usuario int,
 CONSTRAINT pk_usuario_juridico
 	PRIMARY KEY(cd_usuario_juridico),
@@ -135,7 +130,7 @@ CONSTRAINT fk_juridico_usuario
 		REFERENCES tb_usuario(cd_usuario)
 );
 
-CREATE TABLE tb_cidade(		-- 
+CREATE TABLE tb_cidade(
 cd_cidade int not null,
 nm_cidade VARCHAR(255),
 sg_uf char(2),
@@ -157,32 +152,20 @@ CONSTRAINT pk_bairro_cidade
 		REFERENCES tb_cidade(cd_cidade)
 );
 
-CREATE TABLE tb_peca(	-- OK
+CREATE TABLE tb_peca(
 cd_peca int not null,
 nm_peca varchar(255),
 qt_medida int,
 sg_medida char(2),
-cd_tipo_peca int not null,
+cd_tipo_peca int,
 CONSTRAINT pk_peca
 	PRIMARY KEY(cd_peca),
 CONSTRAINT fk_peca_unidade
 	FOREIGN KEY(sg_medida)
 		REFERENCES tb_medida(sg_medida),
-CONSTRAINT fk_categoria_peca
-	FOREIGN KEY(cd_tipo_peca)
+CONSTRAINT fk_peca_tipo
+	FOREIGN KEY (cd_tipo_peca)
 		REFERENCES tb_tipo_peca(cd_tipo_peca)
-);
-
-CREATE TABLE status_peca(	-- OK
-cd_peca int not null,
-sg_status char(2),
-dt_status timestamp,
-CONSTRAINT fK_status_peca
-	FOREIGN KEY(cd_peca)
-		REFERENCES tb_peca(cd_peca),
-CONSTRAINT fK_peca_status
-	FOREIGN KEY(sg_status)
-		REFERENCES tb_status(sg_status)
 );
 
 CREATE TABLE tb_logradouro(
@@ -200,7 +183,25 @@ CONSTRAINT fk_logradouro_bairro
 		REFERENCES tb_bairro(cd_bairro)
 );
 
-CREATE TABLE tb_modelo( -- OK
+CREATE TABLE tb_anuncio(
+cd_anuncio int not null,
+nm_titulo varchar(255),
+ds_anuncio TEXT,
+in_pago BOOLEAN,
+vl_pagar int,
+cd_usuario_juridico int,
+cd_tipo_anuncio int,
+CONSTRAINT pk_anuncio
+	PRIMARY KEY(cd_anuncio),
+CONSTRAINT fk_anuncio_usuario
+	FOREIGN KEY(cd_usuario_juridico)
+		REFERENCES tb_usuario_juridico(cd_usuario_juridico),
+CONSTRAINT fk_anuncio_tipo
+	FOREIGN KEY (cd_tipo_anuncio)
+		REFERENCES tb_tipo_anuncio(cd_tipo_anuncio)
+);
+
+CREATE TABLE tb_modelo(
 cd_modelo int not null,
 nm_modelo VARCHAR(255),
 aa_modelo INT,
@@ -218,6 +219,41 @@ CONSTRAINT fk_categoria
 CONSTRAINT fk_marca
 	FOREIGN KEY(cd_marca)
 		REFERENCES tb_marca(cd_marca)
+);
+
+CREATE TABLE tb_veiculo(
+cd_veiculo int not null,
+nm_cor varchar(255),
+cd_placa varchar(8),
+cd_usuario int,
+cd_modelo int,
+CONSTRAINT pk_veiculo
+	PRIMARY KEY (cd_veiculo),
+CONSTRAINT fk_veiculo_usuario
+	FOREIGN KEY(cd_usuario)
+		REFERENCES tb_usuario(cd_usuario),
+CONSTRAINT fk_veiculo_modelo
+	FOREIGN KEY(cd_modelo)
+		REFERENCES tb_modelo(cd_modelo)
+);
+
+CREATE TABLE tb_check(
+cd_check int not null,
+dt_check timestamp, 
+cd_veiculo int,
+sg_status char(2),
+cd_peca int,
+CONSTRAINT pk_usuario
+	PRIMARY KEY(cd_check),
+CONSTRAINT fk_check_veiculo	
+	FOREIGN KEY(cd_veiculo)
+		REFERENCES tb_veiculo(cd_veiculo),
+CONSTRAINT fk_check_status
+	FOREIGN KEY(sg_status)
+		REFERENCES tb_status(sg_status),
+CONSTRAINT fk_check_peca
+	FOREIGN KEY(cd_peca)
+		REFERENCES tb_peca(cd_peca)
 );
 
 
