ab045ad2ed0bc8a85cb3982b1ba568a6f82be19b
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-09-10 23:17:07 -0300

    css sem link da internet

--- Views/sistemaJ.php
+++ Views/sistemaJ.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
     <link rel="stylesheet" type="text/css"
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        href="/Views/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/sistema.css">
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <title>Revise - Home</title>
