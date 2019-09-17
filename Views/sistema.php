7d0f17a4651d4a0563ce6746940cd1966ab5378d
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-09-16 23:48:01 -0300

    login

--- Views/sistema.php
+++ Views/sistema.php
@@ -20,114 +20,42 @@
     <link rel="stylesheet" type="text/css" href="/Views/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="css/sistema.css">
     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/component.css">
+    <link rel="stylesheet" type="text/css" href="css/demo.css">
+
     <title>Revise - Home</title>
 </head>
 
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-0 col-sm-0 col-md-2 col-lg-2 col-xl-2">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="page-wrapper chiller-theme toggled navbar-brand ">
-                            <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-                                <i class="fas fa-bars"></i>
-                            </a>
-                            <nav id="sidebar" class="sidebar-wrapper">
-                                <div class="sidebar-content">
-                                    <div class="sidebar-brand">
-                                        <a class="navbar-brand" href="#">
-                                            <div class="row">
-                                                <div class="col-4">
-                                                    <img src="img/logo.png" width="50" height="50" alt="Revise"
-                                                        class="d-inline-block align-top">
-                                                </div>
-                                                <div class="col-8">
-                                                    <label class="tituloMenu">Revise</label>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="sidebar-header">
-                                        <div class="user-pic">
-                                            <img class="img-responsive img-rounded" src="<?php echo $FotoDePerfil ?>"
-                                                alt="User picture">
-                                        </div>
-                                        <div class="user-info">
-                                            <span class="user-name"><strong><?php echo $nome ?></strong>
-                                                <?php echo $sobrenome ?>
-                                            </span>
-                                            <span class="user-role"><?php echo $placa ?></span>
-                                            <span class="user-role"><?php echo $modelo ?></span>
-                                        </div>
-                                    </div>
-                                    <!-- sidebar-search  -->
-                                    <div class="sidebar-menu">
-                                        <ul>
-                                            <li class="header-menu">
-                                                <span>Geral</span>
-                                            </li>
-                                            <li class="sidebar-content">
-                                                <a href="#">
-                                                    <i class="fa fa-home fa-fw" aria-hidden="true"></i>
-                                                    <span>Início</span>
-                                                </a>
-                                            </li>
-                                            <li class="sidebar-content">
-                                                <a href="#">
-                                                    <i class="fa fa-user-circle" aria-hidden="true"></i>
-                                                    <span>Perfil</span>
-                                                </a>
-                                            </li>
-                                            <li class="sidebar-content">
-                                                <a href="#">
-                                                    <i class="fa fa-wrench" aria-hidden="true"></i>
-                                                    <span>Histórico de Peças</span>
-                                                    <span
-                                                        class="badge badge-pill badge-danger"><?php echo $peças; ?></span>
-                                                </a>
-                                            </li>
-                                            <li class="sidebar-content">
-                                                <a href="#">
-                                                    <i class="fa fa-car" aria-hidden="true"></i>
-                                                    <span>Veículos</span>
-                                                </a>
-                                            </li>
-                                            <li class="sidebar-content">
-                                                <a href="#">
-                                                    <i class="fa fa-building" aria-hidden="true"></i>
-                                                    <span>Serviços e Produtos</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <!-- sidebar-menu  -->
-                                </div>
-                                <!-- sidebar-content  -->
-                                <div class="sidebar-footer rodapeMenu">
-                                    <a href="#" class="buttonMenuRodape">
-                                        <svg id="i-settings" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"
-                                            width="22" height="22" fill="none" stroke="currentcolor"
-                                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                                            <path
-                                                d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" />
-                                            <circle cx="16" cy="16" r="4" />
-                                        </svg>
-                                    </a>
-                                    <a href="../index.html" class="buttonMenuRodape">
-                                        <svg id="i-signout" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"
-                                            width="22" height="22" fill="none" stroke="currentcolor"
-                                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                                            <path d="M28 16 L8 16 M20 8 L28 16 20 24 M11 28 L3 28 3 4 11 4" />
-                                        </svg>
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <nav class="gn-menu-wrapper">
+                <div class="gn-scroller">
+                    <ul class="gn-menu">
+                        <li class="gn-search-item">
+                            <input placeholder="Search" type="search" class="gn-search">
+                            <a class="gn-icon gn-icon-search"><span><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Procurar</font></font></span></a>
+                        </li>
+                        <li>
+                            <a class="gn-icon gn-icon-download"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Transferências</font></font></a>
+                            <ul class="gn-submenu">
+                                <li><a class="gn-icon gn-icon-illustrator"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ilustrações vetoriais</font></font></a></li>
+                                <li><a class="gn-icon gn-icon-photoshop"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Arquivos do Photoshop</font></font></a></li>
+                            </ul>
+                        </li>
+                        <li><a class="gn-icon gn-icon-cog"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Configurações</font></font></a></li>
+                        <li><a class="gn-icon gn-icon-help"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Socorro</font></font></a></li>
+                        <li>
+                            <a class="gn-icon gn-icon-archive"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Arquivos</font></font></a>
+                            <ul class="gn-submenu">
+                                <li><a class="gn-icon gn-icon-article"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Artigos</font></font></a></li>
+                                <li><a class="gn-icon gn-icon-pictures"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Imagens</font></font></a></li>
+                                <li><a class="gn-icon gn-icon-videos"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Vídeos</font></font></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /gn-scroller -->
+            </nav>
             <div class="col-12 col-sm-12 col-md-10 col-lg-10 col-xl-10" style="background-color: palegreen; padding: 50px;">
                 <div class="card bg-dark" style="width: 30rem; height: 10rem;">
                     <div class="card-body ">
@@ -158,6 +86,9 @@
 <script src="./js/sweetalert.min.js"></script>
 <script src="js/progressbar.js"></script>
 <script src="js/sistema.js"></script>
+<script src="js/classie.js"></script>
+<script src="js/gnmenu.js"></script>
+<script src="js/modernizr.custom.js"></script>
 
 </html>
 
