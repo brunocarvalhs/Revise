a467cdeef67e18b9d470c4aea1e4c78fcde50219
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-09-21 11:15:27 -0300

    Update perfilFisico.php

--- Views/perfilFisico.php
+++ Views/perfilFisico.php
@@ -144,14 +144,14 @@
                                     </div>
                                     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                                         <input type="text" class="form-control" id="campoNome">
-                                           
+
                                     </div>
                                 </div>
                             </div>
                             <!-- E-mail -->
                             <div class="form-group inputCadastro">
                                 <label for="exampleFormControlInput1">Endereço de email</label>
-                                <input type="email" class="form-control" id="campoEmail" >
+                                <input type="email" class="form-control" id="campoEmail">
                             </div>
                             <div class="form-group inputCadastro">
                                 <div class="form-row">
@@ -164,33 +164,51 @@
                                     <!-- Data de nascimento -->
                                     <div class="col-12 col-sm-12 col-md col-lg col-xl">
                                         <label for="exampleFormControlInput1">Data de nascimento</label>
-                                        <input type="date" class="form-control" id="campoDeNascimento"
-                                          >
+                                        <input type="date" class="form-control" id="campoDeNascimento">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-sm-12 col-md col-lg col-xl">
+                                <div class="col-12 col-sm-12 col-md col-lg col-xl">
+                                    <div class="input-group mb-3">
+                                        <select class="custom-select" id="txtplano" name="txtplano"
+                                            aria-label="Exemplo de select com botão addon">
+                                            <option value="1" selected>Basic</option>
+                                            <option value="2">Standart</option>
+                                            <option value="3">Profission</option>
+                                            <option value="4">Deluxe Edition </option>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
                             <!--Botões para alteração-->
                             <br>
                             <div class="row">
-                                <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4" style="display: block;" id="btn_off">
-                                    <button type="button" class="btn btn-warning btn-block" onclick="btnPerfil()">Alterar dados</button>
+                                <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4" style="display: block;"
+                                    id="btn_off">
+                                    <button type="button" class="btn btn-warning btn-block"
+                                        onclick="btnPerfil()">Alterar dados</button>
                                 </div>
                                 <!--Botões escondidos-->
                                 <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8" style="display: none;"
                                     id="btn_hidden">
                                     <div class="row">
-                                            <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5">
-                                                    <button type="button" class="btn btn-warning btn-block" onclick="Salvar()">Salvar</button>
-                                                </div>
-                                                <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5" >
-                                                    <button type="button" class="btn btn-warning btn-block" onclick="Cancelar()">Cancelar</button>
-                                                </div>
+                                        <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                                            <button type="button" class="btn btn-warning btn-block"
+                                                onclick="Salvar()">Salvar</button>
+                                        </div>
+                                        <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                                            <button type="button" class="btn btn-warning btn-block"
+                                                onclick="Cancelar()">Cancelar</button>
+                                        </div>
                                     </div>
-                                   
+
                                 </div>
                             </div>
-                            </div>
-                            </div>
+                        </div>
+                    </div>
+                   
             </form>
         </div>
     </div>
