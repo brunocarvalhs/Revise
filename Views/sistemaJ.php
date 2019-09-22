d676274f052f0b03e43f5146b991a48e9f44b813
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-09-22 17:44:34 -0300

    ajustes

--- Views/sistemaJ.php
+++ Views/sistemaJ.php
@@ -90,41 +90,48 @@
                         </div>
                         <!-- sidebar-menu  -->
                     </div>
-                    <!-- sidebar-content  -->
-                    <div class="sidebar-footer rodapeMenu">
-                        <a href="#" class="buttonMenuRodape">
-                            <svg id="i-settings" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22"
-                                height="22" fill="none" stroke="currentcolor" stroke-linecap="round"
-                                stroke-linejoin="round" stroke-width="2">
-                                <path
-                                    d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" />
-                                <circle cx="16" cy="16" r="4" />
-                            </svg>
-                        </a>
-                        <a href="../index.html" class="buttonMenuRodape">
-                            <svg id="i-signout" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22"
-                                height="22" fill="none" stroke="currentcolor" stroke-linecap="round"
-                                stroke-linejoin="round" stroke-width="2">
-                                <path d="M28 16 L8 16 M20 8 L28 16 20 24 M11 28 L3 28 3 4 11 4" />
-                            </svg>
-                        </a>
+                    <!-- sidebar-content  -->                   
+                    <div class="sidebar-footer rodapeMenu bg-dark">                                                           
+                                <div class="col-9"></div>
+                                <div class="col-2">
+                                    <div class="dropdown">
+                                        <button class="btn btn-dark" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            <svg id="i-settings" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22"
+                                                fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2">
+                                                <path
+                                                    d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" />
+                                                <circle cx="16" cy="16" r="4" />
+                                            </svg>
+                                        </button>
+                                        <div class="dropdown-menu bg-warning" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="">Excluir conta</a>
+                                        </div>
+                                    </div>
+                                </div>                                 
+                              </div>
+                        </nav>
                     </div>
-                </nav>
-            </div>
-            <div class="col">
-                <button></button>
+                </div>
             </div>
         </div>
 </body>
 <script src="js/jquery.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
-<script src="js/wow.js"></script>
-<script src="js/index.js"></script>
-<!-- page-wrapper -->
+<script src="./js/wow.js"></script>
+<script src="./js/index.js"></script>
 <script src="js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
     crossorigin="anonymous"></script>
 <script src="js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
     crossorigin="anonymous"></script>
-    <script src="js/progressbar.js"></script>
-
+<script src="./js/sweetalert.min.js"></script>
+<script src="js/progressbar.js"></script>
+<script src="js/sistema.js"></script>
+<script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+<script src="js/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
 </html>
