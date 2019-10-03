d8dc57a6e7e1b5651fbbb43ad348dacca5c6fcba
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-10-03 00:00:21 -0300

    legenda

--- Views/sistema.php
+++ Views/sistema.php
@@ -146,39 +146,84 @@
                                 </div>
                                 <!-- sidebar-menu  -->
                             </div>
-                <!-- sidebar-content  -->                   
-                <div class="sidebar-footer rodapeMenu bg-dark">                                                           
-                    <div class="col-9"></div>
-                    <div class="col-2">
-                        <div class="dropdown">
-                            <button class="btn btn-dark" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                <svg id="i-settings" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22"
-                                    fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2">
-                                    <path
-                                        d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" />
-                                    <circle cx="16" cy="16" r="4" />
-                                </svg>
-                            </button>
-                            <div class="dropdown-menu bg-warning" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item bg-warning" href="">Excluir conta</a>
-                            </div>
-                        </div>
-                    </div>                                 
+        <!-- sidebar-content  -->                   
+        <div class="sidebar-footer rodapeMenu bg-dark">                                                           
+            <div class="col-9"></div>
+            <div class="col-2">
+                <div class="dropdown">
+                    <button class="btn btn-dark" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        <svg id="i-settings" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22"
+                            fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2">
+                            <path
+                                d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" />
+                            <circle cx="16" cy="16" r="4" />
+                        </svg>
+                    </button>
+                    <div class="dropdown-menu bg-warning" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item bg-warning" href="">Excluir conta</a>
                     </div>
-            </nav>
-        </div>
-    </div>
+                </div>
+            </div>                                 
+            </div>
+    </nav>
 </div>
 </div>
 </div>
-<div class="col-12 col-sm-12 col-md-0 col-lg-9 col-xl-9">
+</div>
+</div>
+<div class="col-12 col-sm-12 col-md-0 col-lg-9 col-xl-9"> 
+<div class="row">
+    <div class="col-1 col-sm-1 col-md-1 col-lg-3 col-xl-3"></div>
+    <div class="col-10 col-sm-10 col-md-10 col-lg-9 col-xl-9" id="divPadding">
+        <!--Legenda-->
+        <div class="card text-white bg-secondary mb-3" style="width: 100%;">
+            <div class="card-header text-center">Legenda</div>
+            <div class="card-body">
+                <div class="row text-center">
+                    <div class="col-3">
+            <div class="progress">
+                <div class="progress-bar bg-light" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                    aria-valuemax="100"></div>
+            </div>    
+            <div class=" text-light">Contagem zerada</div>
+            </div>  
+            <div class="col-3">
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class=" text-light">Longe do vencimento</div>
+            </div>  
+            <div class="col-3">
+                <div class="progress">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="100"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class=" text-light">Ops! Fique atento</div>
+            </div>         
+            <div class="col-3">
+                <div class="progress">
+                    <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0"
+                        aria-valuemax="100"></div>
+                </div>
+                <div class=" text-light">Peça vencida</div>
+            
+            </div>
+            </div>
+            </div>         
+            </div>
+            </div>
+            </div>
+<!--Fim legenda-->
+<br><br>
 <!--Card-->
 <div class="row">
-    <div class="col-1 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-    <div class="col-10 col-sm-8 col-md-8 col-lg-8 col-xl-8" id="divPadding">
-        <div class="card bg-dark w-100" >
+        <div class="col-1 col-sm-1 col-md-1 col-lg-3 col-xl-3"></div>
+        <div class="col-10 col-sm-10 col-md-10 col-lg-9 col-xl-9" id="divPadding">
+
+<div class="card bg-dark w-100" >
 <div class="card-body">
      <!--Título do card-->
     <div class="container" >
@@ -224,8 +269,8 @@
 </div>
 <!--Card-->
 <div class="row">
-        <div class="col-1 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-        <div class="col-10 col-sm-8 col-md-8 col-lg-8 col-xl-8" id="divPadding">
+        <div class="col-1 col-sm-1 col-md-1 col-lg-3 col-xl-3"></div>
+        <div class="col-10 col-sm-10 col-md-10 col-lg-9 col-xl-9" id="divPadding">
             <div class="card bg-dark w-100" >
     <div class="card-body">
          <!--Título do card-->
@@ -273,8 +318,8 @@
     </div>
     <!--Card-->
 <div class="row">
-        <div class="col-1 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-        <div class="col-10 col-sm-8 col-md-8 col-lg-8 col-xl-8" id="divPadding">
+        <div class="col-1 col-sm-1 col-md-1 col-lg-3 col-xl-3"></div>
+    <div class="col-10 col-sm-10 col-md-10 col-lg-9 col-xl-9" id="divPadding">
             <div class="card bg-dark w-100" >
     <div class="card-body">
          <!--Título do card-->
@@ -324,8 +369,8 @@
     
     <!--Card-->
     <div class="row">
-            <div class="col-1 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-            <div class="col-10 col-sm-8 col-md-8 col-lg-8 col-xl-8" id="divPadding">
+            <div class="col-1 col-sm-1 col-md-1 col-lg-3 col-xl-3"></div>
+            <div class="col-10 col-sm-10 col-md-10 col-lg-9 col-xl-9" id="divPadding">
                 <div class="card bg-dark w-100" >
         <div class="card-body">
              <!--Título do card-->
@@ -369,7 +414,7 @@
                 </div>
                 </div>
                 </div>
-            <div class="col-1 col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+            <div class="col-1 col-sm-2 col-md-2 col-lg-2 col-xl-0"></div>
         </div>
     </div>
     </div>
