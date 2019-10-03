ca6549f1689814c5fc01939bfbe389af5dea011c
Katryel Menezes Vieira <katryelmenezesvieira14@gmail.com>
2019-10-03 15:31:50 -0300

    corrigindo

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
-require_once 'Models/modelFIsico.php';
 require_once 'Models/modelUsuario.php';
-require_once 'Lib/connection.php.php';
-$user = new UserFisico;
-$user->VerificarCadastrar('marinalunasales__marinalunasales@acritica.com.br','13213212','EYT-4550');
+$user = new Usuario;
+echo '<pre>';
+print_r($user);
 ?>
