c07e0d7a30de87dbb7edfd533f84c6f806664525
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-06 17:41:11 -0300

    Update sistema.php

--- Views/sistema.php
+++ Views/sistema.php
@@ -436,7 +436,7 @@ $peças = 2;
         }
     </style>
 </body>
-<script src="js/jquery.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+<script src=".js/jquery.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
 <script src="./js/wow.js"></script>
 <script src="./js/index.js"></script>
@@ -447,6 +447,7 @@ $peças = 2;
 <script src="./js/sweetalert.min.js"></script>
 <script src="js/progressbar.js"></script>
 <script src="js/sistema.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
