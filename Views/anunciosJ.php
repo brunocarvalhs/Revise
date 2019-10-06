6a38552777bc07cbfa0c94ff83d0c28bca7cdc95
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-06 17:39:13 -0300

    Menu sistema ajuste

--- Views/anunciosJ.php
+++ Views/anunciosJ.php
@@ -226,9 +226,8 @@ $CNPJ = '12.345.678/9101-11';
 <script src="./js/sweetalert.min.js"></script>
 <script src="./js/progressbar.js"></script>
 <script src="./js/sistema.js"></script>
-<script src="./js/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-<script src="./js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
 </html>
