f212e869925c26071093b9b1aeb1cb8c01562e83
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-08 22:38:48 -0300

    Update Fisico.blade.php

--- resources/views/Fisico.blade.php
+++ resources/views/Fisico.blade.php
@@ -6,7 +6,5 @@
 
 @endsection
 @section('script')
-<script src="js/jquery.min.js" type="text/javascript" async="true" defer></script>
-<script src="js/popper.min.js" type="text/javascript" async="true" defer></script>
-<script src="js/bootstrap.min.js" type="text/javascript" async="true" defer></script>
+
 @endsection
