fb041d65b8f531c11be50018ff456e3df4db35cf
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-09 03:56:01 -0300

    Update fisico.blade.php

--- resources/views/template/fisico.blade.php
+++ resources/views/template/fisico.blade.php
@@ -163,7 +163,7 @@
         </div>
     </div>
 </body>
-<script src="{{asset('js/jquery.min.js')}}" type="text/javascript" async="true" defer></script>
+<script src='js/jquery.min.js' type="text/javascript" async="true" defer></script>
 <script src="{{asset('js/jquery.mask.js')}}" type="text/javascript" async="true" defer></script>
 <script src="{{asset('js/popper.min.js')}}" type="text/javascript" async="true" defer></script>
 <script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript" async="true" defer></script>
