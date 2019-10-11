0ee24ff5214360e55492e75f4d961efc3e387ae7
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-11 15:39:27 -0300

    Update alert.blade.php

--- resources/views/componentes/alert.blade.php
+++ resources/views/componentes/alert.blade.php
@@ -1,8 +1,6 @@
 <div>
     <script>
 
-        const swal = require('/xampp/htdocs/node_modules/sweetalert/typings/sweetalert');
-
         swal("{{ $titulo }}", "{{ $mensagem }}", {
             icon: "{{ $tipo  }}",
             buttons: false,
