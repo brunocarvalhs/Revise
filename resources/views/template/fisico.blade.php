e7d49eacf61ac386e1492a476bc7694431469eab
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-11 02:48:32 -0300

    Update fisico.blade.php

--- resources/views/template/fisico.blade.php
+++ resources/views/template/fisico.blade.php
@@ -34,22 +34,28 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
-                                <a class="nav-link h4">
-                                    <div class="sidebar-header">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <div class="user-pic">
-                                                    <img class="img-responsive img-rounded" src="{{ asset('img/user.jpg') }}" width="50" height="50" alt="Revise" class="d-inline-block align-top alig">
-                                                </div>
-                                                <div class="user-info">
-                                                    <span class="user-name">
-                                                        <strong>{{ $dados->Nome }}</strong>
-                                                    </span>
+                                <div class="row">
+                                    <div class="col-8">
+                                        <a class="nav-link h4">
+                                            <div class="sidebar-header">
+                                                <div class="row">
+                                                    <div class="col-4">
+                                                        <div class="user-pic">
+                                                            <img class="img-responsive img-rounded" src="{{ asset('img/user.jpg') }}" width="50" height="50" alt="Revise" class="d-inline-block align-top alig">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-8">
+                                                        <div class="user-info">
+                                                            <span class="user-name">
+                                                                <strong>{{ $dados->Nome }}</strong>
+                                                            </span>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </a>
                                     </div>
-                                </a>
+                                </div>
                             </li>
                             <li class="nav-item active">
                                 <a class="nav-link h4">Menu</a>
