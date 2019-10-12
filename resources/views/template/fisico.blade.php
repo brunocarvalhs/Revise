f2fc322e5d8709acc146a65e91ab8bf3b40780f1
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 16:34:05 -0300

    IHC

--- resources/views/template/fisico.blade.php
+++ resources/views/template/fisico.blade.php
@@ -17,7 +17,7 @@
 
 <body class="bg-light container-fluid semPadding">
     <div class="row">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-2">
             <!--Menu superior responsivo-->
             <div class="col-12 col-sm-12 col-md-0 col-lg-0 col-xl-0 menusuperior">
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -192,7 +192,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-10 conteudo">
             <div class="container">
                 <div class="row">
                     <div class="col-12">
