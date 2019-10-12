4747522110fb32f3c90c1561dee64ae78de05af1
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 16:12:52 -0300

    Anuncio

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -3,51 +3,28 @@
 @section('titulo','Serviços e Produtos')
 
 @section('style')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/SearchBar.css') }}">
+
 @endsection
 
 @section('sistema_fisico')
-    <div class="s128">
-      <form>
-        <div class="inner-form">
-          <div class="row">
-            <div class="input-field second">
-              <input type="search" placeholder="Keyword" />
+<div class="row">
+    <form>
+        <div class="card">
+            <div class="row">
+                <div class="col-12">
+                    <div class="active-pink-3 active-pink-4 mb-4">
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </form>
-    </div>
+    </form>
+</div>
+<div class="row">
+
+</div>
 @endsection
 
 @section('script')
-        <script>
-      var btnDelete = document.getElementById('clear');
-      var inputFocus = document.getElementById('inputFocus');
-      //- btnDelete.on('click', function(e) {
-      //-   e.preventDefault();
-      //-   inputFocus.classList.add('isFocus')
-      //- })
-      //- inputFocus.addEventListener('click', function() {
-      //-   this.classList.add('isFocus')
-      //- })
-      btnDelete.addEventListener('click', function(e)
-      {
-        e.preventDefault();
-        inputFocus.value = ''
-      })
-      document.addEventListener('click', function(e)
-      {
-        if (document.getElementById('first').contains(e.target))
-        {
-          inputFocus.classList.add('isFocus')
-        }
-        else
-        {
-          // Clicked outside the box
-          inputFocus.classList.remove('isFocus')
-        }
-      });
 
-    </script>
 @endsection
