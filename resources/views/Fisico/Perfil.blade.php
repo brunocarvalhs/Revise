fec442d4087d5b539273a8fb5a015c3f2d09373d
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 19:17:31 -0300

    Update Perfil.blade.php

--- resources/views/Fisico/Perfil.blade.php
+++ resources/views/Fisico/Perfil.blade.php
@@ -15,11 +15,8 @@
 @section('sistema_fisico')
 <div class="card testimonial-card">
     <div class="card-up lighten-1"></div>
-
-    <div class="col-12 col-sm-4 col-md-3 col-lg-3 col-xl-2">
-        <div class="avatar mx-auto">
-            <img src="{{ asset('img/user.jpg') }}" width="300" height="300" class="rounded-circle" alt="woman avatar">
-        </div>
+    <div class="avatar mx-auto row">
+        <img src="{{ asset('img/user.jpg') }}" width="300" height="300" class="rounded-circle" alt="woman avatar">
     </div>
     <div class="card-body">
         <h4 class="card-title">{{ $dados->Nome }}</h4>
