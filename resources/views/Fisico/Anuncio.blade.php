0bd10fd735886efb055c0e373e60d305fa98f6db
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 17:39:12 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -4,33 +4,39 @@
 
 @section('style')
 <style>
-.dropdown.dropdown-lg .dropdown-menu {
-    margin-top: -1px;
-    padding: 6px 20px;
-}
-.input-group-btn .btn-group {
-    display: flex !important;
-}
-.btn-group .btn {
-    border-radius: 0;
-    margin-left: -1px;
-}
-.btn-group .btn:last-child {
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-}
-.btn-group .form-horizontal .btn[type="submit"] {
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-}
-.form-horizontal .form-group {
-    margin-left: 0;
-    margin-right: 0;
-}
-.form-group .form-control:last-child {
-    border-top-left-radius: 4px;
-    border-bottom-left-radius: 4px;
-}
+    .dropdown.dropdown-lg .dropdown-menu {
+        margin-top: -1px;
+        padding: 6px 20px;
+    }
+
+    .input-group-btn .btn-group {
+        display: flex !important;
+    }
+
+    .btn-group .btn {
+        border-radius: 0;
+        margin-left: -1px;
+    }
+
+    .btn-group .btn:last-child {
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+    }
+
+    .btn-group .form-horizontal .btn[type="submit"] {
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+    }
+
+    .form-horizontal .form-group {
+        margin-left: 0;
+        margin-right: 0;
+    }
+
+    .form-group .form-control:last-child {
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+    }
 </style>
 @endsection
 
@@ -42,21 +48,23 @@
                 <input type="text" class="form-control" aria-label="Text input with dropdown button">
                 <div class="input-group-append">
                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Filtros</button>
-                    <div class="dropdown-menu">
-                        <div class="form-group">
-                            <select class="custom-select">
-                                <option value="">Tipo</option>
-                                <option value="1">Serviços</option>
-                                <option value="2">Produtos</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <select class="custom-select">
-                                <option value="">Ordem</option>
-                                <option value="1">Mais Barato</option>
-                                <option value="2">Mais Caros</option>
-                            </select>
-                        </div>
+                    <div class="dropdown-menu dropdown-menu-right" role="menu">
+                        <form class="form-horizontal" role="form">
+                            <div class="form-group">
+                                <select class="custom-select">
+                                    <option value="">Tipo</option>
+                                    <option value="1">Serviços</option>
+                                    <option value="2">Produtos</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <select class="custom-select">
+                                    <option value="">Ordem</option>
+                                    <option value="1">Mais Barato</option>
+                                    <option value="2">Mais Caros</option>
+                                </select>
+                            </div>
+                        </form>
                     </div>
                 </div>
                 <button type="submit" class="input-group-append btn btn-dark">Submit</button>
