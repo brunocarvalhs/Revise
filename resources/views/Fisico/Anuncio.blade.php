7edc895c6136e7b1fee7a8964c10db31da3c2952
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 17:01:06 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -13,7 +13,7 @@
             <div class="active-pink-3 active-pink-4 mb-4">
                 <div class="input-group mb-3 pesquisaInput">
                     <div class="custom-file">
-                        <input class="form-control pesquisaInput" type="text" placeholder="Search" aria-label="Search">
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
                     </div>
                     <div class="input-group-append">
                         <select class="selectpicker">
