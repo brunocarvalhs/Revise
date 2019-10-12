89d2f338d7b26ca47f976e12930dc561d0f459f9
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 16:39:19 -0300

    IHC

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -11,7 +11,7 @@
     <div class="col-12 inputPesquisa">
         <form>
             <div class="active-pink-3 active-pink-4 mb-4">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                <input class="form-control pesquisaInput" type="text" placeholder="Search" aria-label="Search">
             </div>
         </form>
     </div>
