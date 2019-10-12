cc98a4c0185c48fa9faf9cc9890e0a4220446303
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 15:36:45 -0300

    SearchBar

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -11,16 +11,6 @@
       <form>
         <div class="inner-form">
           <div class="row">
-            <div class="input-field first" id="first">
-              <input class="input" id="inputFocus" type="text" placeholder="Keyword" />
-              <button class="clear" id="clear">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div class="row">
             <div class="input-field second">
               <input type="search" placeholder="Keyword" />
             </div>
