8d1d33a8298988cf38b478fb83b2773c3753a641
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 19:39:20 -0300

    Update Perfil.blade.php

--- resources/views/Fisico/Perfil.blade.php
+++ resources/views/Fisico/Perfil.blade.php
@@ -23,7 +23,88 @@
     <div class="card-body">
         <h4 class="card-title">{{ $dados->Nome }}</h4>
         <hr>
-        <p><i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
+        <div class="card-body">
+            <div>
+                <!--Perfil Usuario Fisico-->
+                <form action="" method="POST" onsubmit="">
+                    <div class="container-fluid">
+                        <div class="row formularioCadastro">
+                            <div class="col-12">
+                                <!-- Nome completo -->
+                                <div class="form-group inputCadastro">
+                                    <div class="form-row">
+                                        <div class="col-12">
+                                            <label for="campoNome" class="text-warning">Nome
+                                                completo</label>
+                                        </div>
+                                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                                            <input type="text" class="form-control" id="campoNome">
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- E-mail -->
+                                <div class="form-group inputCadastro">
+                                    <label for="exampleFormControlInput1" class="text-warning">Endereço de
+                                        email</label>
+                                    <input type="email" class="form-control" id="campoEmail">
+                                </div>
+                                <div class="form-group inputCadastro">
+                                    <div class="form-row">
+                                        <!-- CPF -->
+                                        <div class="col-12 col-sm-12 col-md col-lg col-xl">
+                                            <label for="exampleFormControlInput1" class="text-warning">CPF</label>
+                                            <input type="text" class="form-control" id="campoDoCpf" placeholder="xxx.xxx.xxx-xx">
+                                        </div>
+                                        <!-- Data de nascimento -->
+                                        <div class="col-12 col-sm-12 col-md col-lg col-xl">
+                                            <label for="exampleFormControlInput1" class="text-warning">Data
+                                                de nascimento</label>
+                                            <input type="date" class="form-control" id="campoDeNascimento">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                                    <div class="form-row">
+                                        <div class="col-12">
+                                            <label class="text-warning">Planos</label>
+                                        </div>
+                                    </div>
+                                    <div class="input-group mb-3">
+                                        <select class="custom-select" id="txtPlano" name="txtPlano" aria-label="Exemplo de select com botão addon">
+                                            <option value="" selected>Basic</option>
+                                            <option value="" selected>Standart</option>
+                                            <option value="" selected>Profission</option>
+                                            <option value="" selected>Deluxe Edition</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Botões para alteração-->
+                        <br>
+                        <div class="row">
+                            <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4" style="display: block;" id="btn_off">
+                                <button type="button" class="btn btn-outline-warning btn-block" onclick="btnPerfil()">Alterar
+                                    dados</button>
+                            </div>
+                            <!--Botões escondidos-->
+                            <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8" style="display: none;" id="btn_hidden">
+                                <div class="row">
+                                    <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                                        <button type="button" class="btn btn-success btn-block" onclick="Salvar()">Salvar</button>
+                                    </div>
+                                    <div class="col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                                        <button type="button" class="btn btn-danger btn-block" onclick="Cancelar()">Cancelar</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
