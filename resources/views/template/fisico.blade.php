586ec7f8b8c20d4f2f0c53ed2e6e9ece59f13d2f
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 15:39:10 -0300

    IHC

--- resources/views/template/fisico.blade.php
+++ resources/views/template/fisico.blade.php
@@ -193,9 +193,7 @@
         <div class="col-12 col-sm-12 col-md-0 col-lg-9 col-xl-9">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-12">
-                        @yield('sistema_fisico')
-                    </div>
+                    @yield('sistema_fisico')
                 </div>
             </div>
         </div>
--- resources/views/template/juridico.blade.php
+++ resources/views/template/juridico.blade.php
@@ -166,9 +166,7 @@
         <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-12">
-                        @yield('sistema_juridico')
-                    </div>
+                    @yield('sistema_juridico')
                 </div>
             </div>
         </div>
