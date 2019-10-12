a73f634f9c6fb74c6eabed6f0417cb3b27b479e2
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 16:21:21 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -8,7 +8,7 @@
 
 @section('sistema_fisico')
 <div class="row">
-    <div class="col-12">
+    <div class="col-12 card">
         <form>
             <div class="active-pink-3 active-pink-4 mb-4">
                 <input class="form-control" type="text" placeholder="Search" aria-label="Search">
