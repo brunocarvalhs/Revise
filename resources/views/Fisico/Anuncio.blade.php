8e38c59abe0ed9526f3afff8a0d38ce59e11e75b
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 16:55:33 -0300

    Filtro

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -12,6 +12,14 @@
         <form>
             <div class="active-pink-3 active-pink-4 mb-4">
                 <input class="form-control pesquisaInput" type="text" placeholder="Search" aria-label="Search">
+                <div class="input-group mb-3">
+                    <div class="custom-file">
+                        <input class="form-control pesquisaInput" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                    <div class="input-group-append">
+                        <span class="input-group-text" id="inputGroupFileAddon02">Upload</span>
+                    </div>
+                </div>
             </div>
         </form>
     </div>
