3adcf0bca396cd92ea88dcc48fa08c587a738085
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 17:13:00 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -8,16 +8,23 @@
 
 @section('sistema_fisico')
 <div class="row">
-    <div class="col-12 inputPesquisa">
+    <div class="card">
         <form>
-            <div class="active-pink-3 active-pink-4 mb-4">
-                <input class="form-control pesquisaInput" type="text" placeholder="Search" aria-label="Search">
+            <div class="row">
+                <div class="col-12 inputPesquisa">
+
+                    <div class="active-pink-3 active-pink-4 mb-4">
+                        <input class="form-control pesquisaInput" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 inputPesquisa">
+                    @include('componentes\filtro')
+                </div>
             </div>
         </form>
     </div>
-</div>
-<div class="row">
-    @include('componentes\filtro')
 </div>
 @endsection
 
