a20636b182be561db59821d78f5caf2e8fb9f79d
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 15:01:07 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -30,5 +30,33 @@
         @endsection
 
         @section('script')
+        <script>
+      var btnDelete = document.getElementById('clear');
+      var inputFocus = document.getElementById('inputFocus');
+      //- btnDelete.on('click', function(e) {
+      //-   e.preventDefault();
+      //-   inputFocus.classList.add('isFocus')
+      //- })
+      //- inputFocus.addEventListener('click', function() {
+      //-   this.classList.add('isFocus')
+      //- })
+      btnDelete.addEventListener('click', function(e)
+      {
+        e.preventDefault();
+        inputFocus.value = ''
+      })
+      document.addEventListener('click', function(e)
+      {
+        if (document.getElementById('first').contains(e.target))
+        {
+          inputFocus.classList.add('isFocus')
+        }
+        else
+        {
+          // Clicked outside the box
+          inputFocus.classList.remove('isFocus')
+        }
+      });
 
+    </script>
         @endsection
