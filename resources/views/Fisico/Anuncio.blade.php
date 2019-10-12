663a0cacfe91f37c0cc64d3fc970875e97f233e1
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 17:32:33 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -9,32 +9,31 @@
 @section('sistema_fisico')
 <div class="row">
     <div class="col-12 inputPesquisa">
-        <form>
+        <form action="">
             <div class="input-group">
                 <input type="text" class="form-control" aria-label="Text input with dropdown button">
                 <div class="input-group-append">
                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Filtros</button>
                     <div class="dropdown-menu">
-                        <form action="{{ url('/') }}" class="container">
-                            <div class="form-group">
-                                <select class="custom-select">
-                                    <option value="">Tipo</option>
-                                    <option value="1">Serviços</option>
-                                    <option value="2">Produtos</option>
-                                </select>
-                                <div class="invalid-feedback">Example invalid custom select feedback</div>
-                            </div>
-                            <div class="form-group">
-                                <select class="custom-select">
-                                    <option value="">Ordem</option>
-                                    <option value="1">Mais Barato</option>
-                                    <option value="2">Mais Caros</option>
-                                </select>
-                                <div class="invalid-feedback">Example invalid custom select feedback</div>
-                            </div>
-                        </form>
+                        <div class="form-group">
+                            <select class="custom-select">
+                                <option value="">Tipo</option>
+                                <option value="1">Serviços</option>
+                                <option value="2">Produtos</option>
+                            </select>
+                            <div class="invalid-feedback">Example invalid custom select feedback</div>
+                        </div>
+                        <div class="form-group">
+                            <select class="custom-select">
+                                <option value="">Ordem</option>
+                                <option value="1">Mais Barato</option>
+                                <option value="2">Mais Caros</option>
+                            </select>
+                            <div class="invalid-feedback">Example invalid custom select feedback</div>
+                        </div>
                     </div>
                 </div>
+                <button type="submit" class="input-group-append btn btn-dark">Submit</button>
             </div>
         </form>
     </div>
