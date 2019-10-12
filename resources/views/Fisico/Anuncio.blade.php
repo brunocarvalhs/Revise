521ed7362eacbe78f16d05b21cc0ba80f21a10a9
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 17:44:50 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -3,41 +3,7 @@
 @section('titulo','Serviços e Produtos')
 
 @section('style')
-<style>
-    .dropdown.dropdown-lg .dropdown-menu {
-        margin-top: -1px;
-        padding: 6px 20px;
-    }
 
-    .input-group-btn .btn-group {
-        display: flex !important;
-    }
-
-    .btn-group .btn {
-        border-radius: 0;
-        margin-left: -1px;
-    }
-
-    .btn-group .btn:last-child {
-        border-top-right-radius: 4px;
-        border-bottom-right-radius: 4px;
-    }
-
-    .btn-group .form-horizontal .btn[type="submit"] {
-        border-top-left-radius: 4px;
-        border-bottom-left-radius: 4px;
-    }
-
-    .form-horizontal .form-group {
-        margin-left: 0;
-        margin-right: 0;
-    }
-
-    .form-group .form-control:last-child {
-        border-top-left-radius: 4px;
-        border-bottom-left-radius: 4px;
-    }
-</style>
 @endsection
 
 @section('sistema_fisico')
@@ -46,37 +12,26 @@
         <form action="">
             <div class="input-group">
                 <input type="text" class="form-control" aria-label="Text input with dropdown button">
-                <div class="input-group-btn">
-                    <div class="btn-group" role="group">
-                        <div class="dropdown dropdown-lg">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span></button>
-                            <div class="dropdown-menu dropdown-menu-right" role="menu">
-                                <form class="form-horizontal" role="form">
-                                    <div class="form-group">
-                                        <label for="filter">Filter by</label>
-                                        <select class="form-control">
-                                            <option value="0" selected>All Snippets</option>
-                                            <option value="1">Featured</option>
-                                            <option value="2">Most popular</option>
-                                            <option value="3">Top rated</option>
-                                            <option value="4">Most commented</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="contain">Author</label>
-                                        <input class="form-control" type="text" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="contain">Contains the words</label>
-                                        <input class="form-control" type="text" />
-                                    </div>
-                                    <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-                                </form>
-                            </div>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Filtros</button>
+                    <div class="dropdown-menu">
+                        <div class="form-group">
+                            <select class="custom-select">
+                                <option value="">Tipo</option>
+                                <option value="1">Serviços</option>
+                                <option value="2">Produtos</option>
+                            </select>
                         </div>
-                        <button type="submit" class="input-group-append btn btn-dark">Submit</button>
+                        <div class="form-group">
+                            <select class="custom-select">
+                                <option value="">Ordem</option>
+                                <option value="1">Mais Barato</option>
+                                <option value="2">Mais Caros</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
+                <button type="submit" class="input-group-append btn btn-dark">Pesquisar</button>
             </div>
         </form>
     </div>
