04b6d29a1b55580e07174f041ec45cedf2973f48
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 19:04:48 -0300

    Update Perfil.blade.php

--- resources/views/Fisico/Perfil.blade.php
+++ resources/views/Fisico/Perfil.blade.php
@@ -13,11 +13,22 @@
 
 
 @section('sistema_fisico')
-
+<div class="card testimonial-card">
+    <div class="card-up indigo lighten-1"></div>
+    <div class="avatar mx-auto white">
+        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle" alt="woman avatar">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">Anna Doe</h4>
+        <hr>
+        <p><i class="fas fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
+    </div>
+</div>
 @endsection
 
 
 
 @section('script')
+
 
 @endsection
