2bc057b6af68fd11d894a6838eed0dfe13c52b70
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 15:54:41 -0300

    IHC

--- resources/views/template/fisico.blade.php
+++ resources/views/template/fisico.blade.php
@@ -195,7 +195,7 @@
         <div class="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-12 semPadding">
+                    <div class="col-12">
                         @yield('sistema_fisico')
                     </div>
                 </div>
