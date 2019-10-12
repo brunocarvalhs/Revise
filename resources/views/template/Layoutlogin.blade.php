5eddd0d1740edbaae88417705d0a9bf5bca5ca90
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 12:37:50 -0300

    Update Layoutlogin.blade.php

--- resources/views/template/Layoutlogin.blade.php
+++ resources/views/template/Layoutlogin.blade.php
@@ -29,27 +29,29 @@
 <script src="/vendor/components/jquery/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{ asset('js/jquery.mask.js') }}"></script>
 <script src="{{ asset('js/index.js') }}"></script>
 <script src="/node_modules/sweetalert/dist/sweetalert.min.js" type="text/javascript" async="true" defer></script>
 <script src="/node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
 <script type="text/javascript" src="//assets.locaweb.com.br/locastyle/edge/javascripts/locastyle.js"></script>
 <script>
     $(document).ready(function () {
-        $("#cpfcnpj").keypress(function () {
+        $("#cpfcnpj").keydown(function(){
+            try {
+                $("#cpfcnpj").unmask();
+            } catch (e) {}
 
             var tamanho = $("#cpfcnpj").val().length;
 
-            if (tamanho < 11)
-            {
-                    $("#cpfcnpj").attr('ls-mask-cnpj', 'ls-mask-cpf');
-            } else
-            {
-                    $("#cpfcnpj").attr('ls-mask-cpf', 'ls-mask-cnpj');
+            if(tamanho < 11){
+                $("#cpfcnpj").mask("999.999.999-99");
+            } else {
+                $("#cpfcnpj").mask("99.999.999/9999-99");
             }
 
             // ajustando foco
             var elem = this;
-            setTimeout(function () {
+            setTimeout(function(){
                 // mudo a posição do seletor
                 elem.selectionStart = elem.selectionEnd = 10000;
             }, 0);
@@ -57,7 +59,7 @@
             var currentValue = $(this).val();
             $(this).val('');
             $(this).val(currentValue);
-        })
+        });
     });
 
 </script>
