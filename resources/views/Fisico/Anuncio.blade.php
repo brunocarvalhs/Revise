8b76dbc41e2b413b3926e8fd677fa2d0f57b42d3
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-12 18:02:13 -0300

    Update Anuncio.blade.php

--- resources/views/Fisico/Anuncio.blade.php
+++ resources/views/Fisico/Anuncio.blade.php
@@ -16,10 +16,12 @@
                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Filtros</button>
                     <div class="dropdown-menu">
                         <div class="form-group">
+                            <label>Tipos</label>
+
                             <div class="form-check">
                                 <div class="dropdown-item">
                                     <input class="form-check-input" type="radio" name="tipo" id="tipo1" value="todos" checked>
-                                    <label class="form-check-label" for="exampleRadios1">
+                                    <label class="form-check-label" for="tipo1">
                                         Todos
                                     </label>
                                 </div>
@@ -27,7 +29,7 @@
                             <div class="form-check">
                                 <div class="dropdown-item">
                                     <input class="form-check-input" type="radio" name="tipo" id="tipo2" value="servico">
-                                    <label class="form-check-label" for="exampleRadios2">
+                                    <label class="form-check-label" for="tipo2">
                                         Serviços
                                     </label>
                                 </div>
@@ -35,7 +37,7 @@
                             <div class="form-check">
                                 <div class="dropdown-item">
                                     <input class="form-check-input" type="radio" name="tipo" id="tipo3" value="produto">
-                                    <label class="form-check-label" for="exampleRadios2">
+                                    <label class="form-check-label" for="tipo3">
                                         Produtos
                                     </label>
                                 </div>
@@ -43,10 +45,11 @@
                         </div>
                         <div class="dropdown-divider"></div>
                         <div class="form-group">
+                            <label>Preços</label>
                             <div class="form-check">
                                 <div class="dropdown-item">
                                     <input class="form-check-input" type="radio" name="preco" id="preco1" value="todos" checked>
-                                    <label class="form-check-label" for="exampleRadios1">
+                                    <label class="form-check-label" for="preco1">
                                         Todos
                                     </label>
                                 </div>
@@ -54,7 +57,7 @@
                             <div class="form-check">
                                 <div class="dropdown-item">
                                     <input class="form-check-input" type="radio" name="preco" id="preco2" value="menor">
-                                    <label class="form-check-label" for="exampleRadios2">
+                                    <label class="form-check-label" for="preco2">
                                         Menores preços
                                     </label>
                                 </div>
@@ -62,7 +65,7 @@
                             <div class="form-check">
                                 <div class="dropdown-item">
                                     <input class="form-check-input" type="radio" name="preco" id="preco3" value="maior">
-                                    <label class="form-check-label" for="exampleRadios2">
+                                    <label class="form-check-label" for="preco3">
                                         Maiores preços
                                     </label>
                                 </div>
