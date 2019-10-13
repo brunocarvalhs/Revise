35debb4a8581c8fcca8c21ca2240c5f930e7591f
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-13 13:56:24 -0300

    Update Veiculo.blade.php

--- resources/views/Fisico/Veiculo.blade.php
+++ resources/views/Fisico/Veiculo.blade.php
@@ -7,18 +7,42 @@
 
 
 @section('style')
+<style>
+.app {
+    display: grid;
+    grid-template-columns: 20px 1fr 20px;
+}
 
+.app>* {
+    grid-column: 2 / -2;
+}
+
+.app>.full {
+    grid-column: 1 / -1;
+}
+
+.hs {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(6, calc(50% - 40px));
+    grid-template-rows: minmax(150px, 1fr);
+}
+</style>
 @endsection
 
 
 
 
 @section('sistema_fisico')
-<ul>
-    @foreach ($Veiculos as $Veiculo)
-        <li>{{ $Veiculo->placa }} - {{ $Veiculo->modelo }}</li>
-    @endforeach
-</ul>
+<div class="row">
+    <div class="app">
+        <ul class="hs">
+            @foreach ($Veiculos as $Veiculo)
+                <li>{{ $Veiculo->placa }} / {{ $Veiculo->modelo }}</li>
+            @endforeach
+        </ul>
+  </div>
+</div>
 @endsection
 
 
