771f9f7acbd170e1273a9b7bb770beef71e3adc9
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-13 15:00:59 -0300

    Update Veiculo.blade.php

--- resources/views/Fisico/Veiculo.blade.php
+++ resources/views/Fisico/Veiculo.blade.php
@@ -57,12 +57,14 @@
             <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
                 <div class="MultiCarousel-inner">
                     @foreach ($Veiculos as $Veiculo)
+                    <a href="{{ url('/Home/Veiculo/'+$Veiculo->placa) }}">
                         <div class="item">
                             <div class="pad15">
                                 <p class="lead">{{ $Veiculo->placa }}</p>
                                 <p>{{ $Veiculo->modelo }}</p>
                             </div>
                         </div>
+                    </a>
                     @endforeach
                 </div>
                 <button class="btn btn-primary leftLst"><</button>
