34d00876b373635c4526196371f457b331607cad
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-13 20:27:34 -0300

    Update controllerAnuncio.php

--- app/Http/Controllers/controllerAnuncio.php
+++ app/Http/Controllers/controllerAnuncio.php
@@ -16,7 +16,6 @@ class controllerAnuncio extends Controller
         $pesquisa = $request->pesquisa;
         $tipo = $request->tipo;
         $preco = $request->preco;
-        return dd($request);
         $lista = $modelAnuncio->PesquisaDeAnuncio($pesquisa,$tipo,$preco);
         $lista = json_decode($lista);
         return view('Fisico\Anuncio',['Fisico' => $modelFisico, 'Anuncios' => $lista]);
