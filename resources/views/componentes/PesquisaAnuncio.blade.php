4ae3452df377bedf10e8f5f863ec483c6fb58f0a
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-13 20:11:07 -0300

    Update PesquisaAnuncio.blade.php

--- resources/views/componentes/PesquisaAnuncio.blade.php
+++ resources/views/componentes/PesquisaAnuncio.blade.php
@@ -1,16 +1,15 @@
-    <div class="col-12">
-        <li class="list-group-item shadow-sm mb-5 bg-white rounded w-100 semPadding">
-            <div class="card rounded">
-                <div class="card-horizontal">
-                    <div class="card-body">
-                        <h6 class="card-subtitle">{{ $tipo }}</h6>
-                        <h4 class="card-title">{{ $titulo }}</h4>
-                        <p class="card-text">{{ $descricao }}</p>
-                        <h6 class="card-title">Local: {{ $cidade }} - {{ $uf }}</h6>
-                        <h5>{{ $preco }}</h5>
-                        <a class="btn btn-outline-warning  w-100" href="#" role="button"> Mais Detalhes +</a>
-                    </div>
-                </div>
+<li class="list-group-item shadow-sm mb-5 bg-white rounded w-100 semPadding">
+    <div class="card rounded">
+        <div class="card-horizontal">
+            <div class="card-body">
+                <h6 class="card-subtitle">{{ $tipo }}</h6>
+                <h4 class="card-title">{{ $titulo }}</h4>
+                <p class="card-text">{{ $descricao }}</p>
+                <h6 class="card-title">Local: {{ $cidade }} - {{ $uf }}</h6>
+                <h5>{{ $preco }}</h5>
+                <a class="btn btn-outline-warning  w-100" href="#" role="button"> Mais Detalhes +</a>
             </div>
-        </li>
+        </div>
     </div>
+</li>
+
