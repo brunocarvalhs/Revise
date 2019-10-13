ea8840eb23bdfbd14dbf095e1c29910410bc9310
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-13 13:58:58 -0300

    Update Veiculo.blade.php

--- resources/views/Fisico/Veiculo.blade.php
+++ resources/views/Fisico/Veiculo.blade.php
@@ -8,25 +8,70 @@
 
 @section('style')
 <style>
+
+:root {
+  --gutter: 20px;
+}
+
 .app {
-    display: grid;
-    grid-template-columns: 20px 1fr 20px;
+  padding: var(--gutter) 0;
+  display: grid;
+  grid-gap: var(--gutter) 0;
+  grid-template-columns: var(--gutter) 1fr var(--gutter);
+  align-content: start;
 }
 
-.app>* {
-    grid-column: 2 / -2;
+.app > * {
+  grid-column: 2 / -2;
 }
 
-.app>.full {
-    grid-column: 1 / -1;
+.app > .full {
+  grid-column: 1 / -1;
 }
 
 .hs {
-    display: grid;
-    grid-gap: 10px;
-    grid-template-columns: repeat(6, calc(50% - 40px));
-    grid-template-rows: minmax(150px, 1fr);
+  display: grid;
+  grid-gap: calc(var(--gutter) / 2);
+  grid-template-columns: repeat(6, calc(50% - var(--gutter) * 2));
+  grid-template-rows: minmax(150px, 1fr);
+
+  overflow-x: scroll;
+  scroll-snap-type: x proximity;
+  padding-bottom: calc(.75 * var(--gutter));
+  margin-bottom: calc(-.25 * var(--gutter));
 }
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+h1,
+h2,
+h3 {
+  margin: 0;
+}
+
+.app {
+  width: 375px;
+  height: 667px;
+  background: #DBD0BC;
+  overflow-y: scroll;
+}
+
+.hs > li,
+.item {
+  scroll-snap-align: center;
+  padding: calc(var(--gutter) / 2 * 1.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  border-radius: 8px;
+}
+
+
 </style>
 @endsection
 
