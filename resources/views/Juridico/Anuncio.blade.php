266f911b0e3af8ef1a89a816d40bcac917815e86
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-20 13:25:43 -0300

    Update Anuncio.blade.php

--- resources/views/Juridico/Anuncio.blade.php
+++ resources/views/Juridico/Anuncio.blade.php
@@ -16,148 +16,83 @@
 
 
 @section('sistema_juridico')
-<div class="col">
-<!-- Table with panel -->
-<div class="card card-cascade narrower">
-    <!--Card image-->
-    <div
-      class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
-      <div>
-        <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-          <i class="fas fa-th-large mt-0"></i>
-        </button>
-        <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-          <i class="fas fa-columns mt-0"></i>
-        </button>
-      </div>
-      <a href="" class="white-text mx-3">Table name</a>
-      <div>
-        <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-          <i class="fas fa-pencil-alt mt-0"></i>
-        </button>
-        <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-          <i class="far fa-trash-alt mt-0"></i>
-        </button>
-        <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-          <i class="fas fa-info-circle mt-0"></i>
-        </button>
-      </div>
+<div class="col-12">
+    <div class="card">
+        <div class="card-body">
+            This is some text within a panel body.
+        </div>
     </div>
-    <!--/Card image-->
-    <div class="px-4">
-      <div class="table-wrapper">
-        <!--Table-->
-        <table class="table table-hover mb-0">
-          <!--Table head-->
-          <thead>
+</div>
+<div class="col-12">
+    <table class="table table-hover table-fixed">
+        <thead>
             <tr>
-              <th>
-                <input class="form-check-input" type="checkbox" id="checkbox">
-                <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
-              </th>
-              <th class="th-lg">
-                <a>First Name
-                  <i class="fas fa-sort ml-1"></i>
-                </a>
-              </th>
-              <th class="th-lg">
-                <a href="">Last Name
-                  <i class="fas fa-sort ml-1"></i>
-                </a>
-              </th>
-              <th class="th-lg">
-                <a href="">Username
-                  <i class="fas fa-sort ml-1"></i>
-                </a>
-              </th>
-              <th class="th-lg">
-                <a href="">Username
-                  <i class="fas fa-sort ml-1"></i>
-                </a>
-              </th>
-              <th class="th-lg">
-                <a href="">Username
-                  <i class="fas fa-sort ml-1"></i>
-                </a>
-              </th>
-              <th class="th-lg">
-                <a href="">Username
-                  <i class="fas fa-sort ml-1"></i>
-                </a>
-              </th>
+                <th>#</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Country</th>
+                <th>City</th>
+                <th>Position</th>
+                <th>Age</th>
             </tr>
-          </thead>
-          <!--Table head-->
-          <!--Table body-->
-          <tbody>
+        </thead>
+        <tbody>
             <tr>
-              <th scope="row">
-                <input class="form-check-input" type="checkbox" id="checkbox1">
-                <label class="form-check-label" for="checkbox1" class="label-table"></label>
-              </th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+                <th scope="row">4</th>
+                <td>Jerry</td>
+                <td>Horwitz</td>
+                <td>Italy</td>
+                <td>Bari</td>
+                <td>Editor-in-chief</td>
+                <td>41</td>
             </tr>
             <tr>
-              <th scope="row">
-                <input class="form-check-input" type="checkbox" id="checkbox2">
-                <label class="form-check-label" for="checkbox2" class="label-table"></label>
-              </th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
+                <th scope="row">5</th>
+                <td>Janis</td>
+                <td>Joplin</td>
+                <td>Poland</td>
+                <td>Warsaw</td>
+                <td>Video Maker</td>
+                <td>39</td>
             </tr>
             <tr>
-              <th scope="row">
-                <input class="form-check-input" type="checkbox" id="checkbox3">
-                <label class="form-check-label" for="checkbox3" class="label-table"></label>
-              </th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
+                <th scope="row">6</th>
+                <td>Gary</td>
+                <td>Winogrand</td>
+                <td>Germany</td>
+                <td>Berlin</td>
+                <td>Photographer</td>
+                <td>37</td>
             </tr>
             <tr>
-              <th scope="row">
-                <input class="form-check-input" type="checkbox" id="checkbox4">
-                <label class="form-check-label" for="checkbox4" class="label-table"></label>
-              </th>
-              <td>Paul</td>
-              <td>Topolski</td>
-              <td>@P_Topolski</td>
-              <td>Paul</td>
-              <td>Topolski</td>
-              <td>@P_Topolski</td>
+                <th scope="row">7</th>
+                <td>Angie</td>
+                <td>Smith</td>
+                <td>USA</td>
+                <td>San Francisco</td>
+                <td>Teacher</td>
+                <td>52</td>
             </tr>
             <tr>
-              <th scope="row">
-                <input class="form-check-input" type="checkbox" id="checkbox5">
-                <label class="form-check-label" for="checkbox5" class="label-table"></label>
-              </th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
+                <th scope="row">8</th>
+                <td>John</td>
+                <td>Mattis</td>
+                <td>France</td>
+                <td>Paris</td>
+                <td>Actor</td>
+                <td>28</td>
             </tr>
-          </tbody>
-          <!--Table body-->
-        </table>
-        <!--Table-->
-      </div>
-    </div>
-  </div>
-  <!-- Table with panel -->
+            <tr>
+                <th scope="row">9</th>
+                <td>Otto</td>
+                <td>Morris</td>
+                <td>Germany</td>
+                <td>Munich</td>
+                <td>Singer</td>
+                <td>35</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 @endsection
 
