3ae74a08d9f89d024885872a6d52747eacbe704a
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-20 13:35:44 -0300

    Update Anuncio.blade.php

--- resources/views/Juridico/Anuncio.blade.php
+++ resources/views/Juridico/Anuncio.blade.php
@@ -17,25 +17,24 @@
 
 @section('sistema_juridico')
 <div id="main" class="container-fluid" style="margin-top: 50px">
-
     <div id="top" class="row">
+        <div class="col-sm-3">
+            <h2>Anuncios</h2>
+        </div>
+        <form action="" method="POST">
+        <div class="col-sm-6">
+            <div class="input-group h2">
+                <input class="form-control" id="search" type="text" placeholder="Pesquisar Anuncio">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </div>
+       </form>
        <div class="col-sm-3">
-           <h2>Itens</h2>
-       </div>
-       <div class="col-sm-6">
-
-           <div class="input-group h2">
-               <input name="data[search]" class="form-control" id="search" type="text" placeholder="Pesquisar Itens">
-               <span class="input-group-btn">
-                   <button class="btn btn-primary" type="submit">
-                       <span class="glyphicon glyphicon-search"></span>
-                   </button>
-               </span>
-           </div>
-
-       </div>
-       <div class="col-sm-3">
-           <a href="add.html" class="btn btn-primary pull-right h2">Novo Item</a>
+           <button class="btn btn-primary pull-right h2">Novo Item<button>
        </div>
    </div> <!-- /#top -->
 
@@ -61,108 +60,9 @@
                    <td>Jes</td>
                    <td>01/01/2015</td>
                    <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1002</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1003</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1004</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1005</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1006</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1007</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1008</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1009</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-                   </td>
-               </tr>
-               <tr>
-                   <td>1010</td>
-                   <td>Lorem ipsum dolor sit amet, consectetur adipiscing</td>
-                   <td>Jes</td>
-                   <td>01/01/2015</td>
-                   <td class="actions">
-                       <a class="btn btn-success btn-xs" href="view.html">Visualizar</a>
-                       <a class="btn btn-warning btn-xs" href="edit.html">Editar</a>
-                       <a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
+                       <button class="btn btn-success btn-xs" href="view.html">Visualizar<button>
+                       <button class="btn btn-warning btn-xs" href="edit.html">Editar<button>
+                       <button class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir<button>
                    </td>
                </tr>
            </tbody>
