f11ef52ef25c5fa0da10e5af16c2bb48f7212337
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-20 14:17:59 -0300

    Update Anuncio.blade.php

--- resources/views/Juridico/Anuncio.blade.php
+++ resources/views/Juridico/Anuncio.blade.php
@@ -29,32 +29,32 @@
     </div>
 </div>
 <div class="shadow-sm p-3 mb-5 rounded">
-    <button class="btn">Novo Anuncio</button>
+    <button class="btn btn">Novo Anuncio</button>
 </div>
 <div id="list" class="row">
     <div class="table-responsive col-md-12">
         <table class="table table-striped" cellspacing="0" cellpadding="0">
-            <thead>
+            <thead class="col-12">
                 <tr>
-                    <th><input type="checkbox" name="select-all" id="select-all"/></th>
-                    <th>Titulo</th>
-                    <th>Preço</th>
-                    <th>Data de Publicação</th>
-                    <th>Validade</th>
-                    <th>Tipo</th>
-                    <th class="actions">Ações</th>
+                    <th class="col-12"><input type="checkbox" name="select-all" id="select-all"/></th>
+                    <th class="col-12">Titulo</th>
+                    <th class="col-12">Preço</th>
+                    <th class="col-12">Data de Publicação</th>
+                    <th class="col-12">Validade</th>
+                    <th class="col-12">Tipo</th>
+                    <th class="col-12 actions">Ações</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="row">
                 @foreach ($Anuncios as $Anuncios)
                     <tr>
-                        <td><input type="checkbox" value="{{ $Anuncios->ID }}" id="txtIdAnuncio" name="txtIdAnuncio{{ $Anuncios->ID }}"></td>
-                        <td>{{ $Anuncios->Titulo }}</td>
-                        <td>{{ $Anuncios->Valor }}</td>
-                        <td>{{ $Anuncios->Data }}</td>
-                        <td>{{ $Anuncios->Validade }}</td>
-                        <td>{{ $Anuncios->Tipo }}</td>
-                        <td class="actions">
+                        <td class="col-12"><input type="checkbox" value="{{ $Anuncios->ID }}" id="txtIdAnuncio" name="txtIdAnuncio{{ $Anuncios->ID }}"></td>
+                        <td class="col-12">{{ $Anuncios->Titulo }}</td>
+                        <td class="col-12">{{ $Anuncios->Valor }}</td>
+                        <td class="col-12">{{ $Anuncios->Data }}</td>
+                        <td class="col-12">{{ $Anuncios->Validade }}</td>
+                        <td class="col-12">{{ $Anuncios->Tipo }}</td>
+                        <td class="col-12" class="actions">
                             <button class="btn btn-success">Visualiar</button>
                             <button class="btn btn-warning">Editar</button>
                             <button class="btn btn-danger">Excluir</button>
