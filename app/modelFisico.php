874a4fdf0590f9e947df404327f34a4db4eb370d
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-20 12:44:39 -0300

    Update modelFisico.php

--- app/modelFisico.php
+++ app/modelFisico.php
@@ -108,14 +108,14 @@ class modelFisico extends modelUsuario
                 ->where('tb_usuario_fisico.cd_cpf', '=', $CPF, 'and', 'tb_usuario.cd_senha', '=', $SENHA)
                 ->first();
 
-            if (base64_decode($resultado->CPF) == $CPF && base64_decode($resultado->Senha) == $SENHA && $resultado != null) {
-                $this->setSenha(base64_decode($resultado->Senha));
-                $this->setEmail(base64_decode($resultado->Email));
+            if ($resultado->CPF == $CPF && $resultado->Senha == $SENHA && $resultado != null) {
+                $this->setSenha($resultado->Senha);
+                $this->setEmail($resultado->Email);
                 $this->setIdUsuario($resultado->Id);
                 $this->setIdFisico($resultado->Usuario);
-                $this->setNomeFisico(base64_decode($resultado->Nome));
-                $this->setCPF(base64_decode($resultado->CPF));
-                $this->setDataNascimento(base64_decode($resultado->Nascimento));
+                $this->setNomeFisico($resultado->Nome);
+                $this->setCPF($resultado->CPF);
+                $this->setDataNascimento($resultado->Nascimento);
                 return $resultado;
             } else {
                 return false;
@@ -129,31 +129,27 @@ class modelFisico extends modelUsuario
     {
         try {
             if ($request->txtsenha == $request->txtcsenha) {
-
                 $resultado = DB::table('tb_usuario_fisico')
                     ->join('tb_usuario', 'tb_usuario.cd_usuario', '=', 'tb_usuario_fisico.cd_usuario')
                     ->where('tb_usuario_fisico.cd_cpf', '=', $CPF, 'or', 'tb_usuario.nm_email', '=', $request->txtemail)
                     ->exists();
-
                 if (!($resultado)) {
-
                     $auto_usuario = DB::table('tb_usuario')->count() + 1;
                     $auto_usuario_fisico = DB::table('tb_usuario_fisico')->count() + 1;
                     $auto_controle_plano = DB::table('tb_controle_plano')->count() + 1;
-
                     DB::table('tb_usuario')->insert(
                         [
                             'cd_usuario' => $auto_usuario,
-                            'nm_email' => base64_encode($request->txtemail),
-                            'cd_senha' => base64_encode($request->txtsenha),
+                            'nm_email' => base_encode($request->txtemail),
+                            'cd_senha' => $request->txtsenha,
                             'cd_tipo_usuario' => 1
                         ]
                     );
                     DB::table('tb_usuario_fisico')->insert(
                         [
                             'cd_usuario_fisico' => $auto_usuario_fisico,
-                            'nm_usuario_fisico' => base64_encode($request->txtnome),
-                            'cd_cpf' => base64_encode($CPF),
+                            'nm_usuario_fisico' => $request->txtnome,
+                            'cd_cpf' => $CPF,
                             'cd_usuario' => $auto_usuario,
                             'dt_nascimento' => $request->txtNascimento
                         ]
