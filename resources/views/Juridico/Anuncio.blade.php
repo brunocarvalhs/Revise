e0317e246f11e6cb15a9a075285e2597ef27077b
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-20 13:43:10 -0300

    Update Anuncio.blade.php

--- resources/views/Juridico/Anuncio.blade.php
+++ resources/views/Juridico/Anuncio.blade.php
@@ -21,68 +21,6 @@
         <form action="{{ route('pesquisa') }}">
             <div class="input-group">
                 <input type="text" class="form-control" name="pesquisa">
-                <div class="input-group-append">
-                    <button class="btn btn-secondary dropdown-toggle text-light" type="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="true">Filtros</button>
-                    <div class="dropdown-menu">
-                        <div class="form-group">
-                            <label class="dropdown-item">Tipos</label>
-                            <div class="form-check">
-                                <div class="dropdown-item">
-                                    <input class="form-check-input" type="radio" name="tipo" id="tipo1" value=""
-                                        checked>
-                                    <label class="form-check-label" for="tipo1">
-                                        Todos
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-check">
-                                <div class="dropdown-item">
-                                    <input class="form-check-input" type="radio" name="tipo" id="tipo2" value="servico">
-                                    <label class="form-check-label" for="tipo2">
-                                        Serviços
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-check">
-                                <div class="dropdown-item">
-                                    <input class="form-check-input" type="radio" name="tipo" id="tipo3" value="produto">
-                                    <label class="form-check-label" for="tipo3">
-                                        Produtos
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="dropdown-divider"></div>
-                        <div class="form-group">
-                            <label class="dropdown-item">Preços</label>
-                            <div class="form-check">
-                                <div class="dropdown-item">
-                                    <input class="form-check-input" type="radio" name="preco" id="preco1" value=""
-                                        checked>
-                                    <label class="form-check-label" for="preco1">
-                                        Todos
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-check">
-                                <div class="dropdown-item">
-                                    <input class="form-check-input" type="radio" name="preco" id="preco2" value="menor">
-                                    <label class="form-check-label" for="preco2">
-                                        Menores preços
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-check">
-                                <div class="dropdown-item">
-                                    <input class="form-check-input" type="radio" name="preco" id="preco3" value="maior">
-                                    <label class="form-check-label" for="preco3">
-                                        Maiores preços
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <button type="submit" class="input-group-append btn btn-success rounded-right">Pesquisar</button>
             </div>
