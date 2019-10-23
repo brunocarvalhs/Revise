c98fb38b80fc5d803c4aca73fc933e75cb6a1188
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-23 17:35:33 -0300

    Update controllerCheck.php

--- app/Http/Controllers/controllerCheck.php
+++ app/Http/Controllers/controllerCheck.php
@@ -14,12 +14,6 @@ class controllerCheck extends Controller
 
     public function VeificarVeiculo(Request $request,modelCheck $modelCheck, controllerUsuario $controllerUsuario){
         return dd($request);
-        $modelFisico = session()->get('Fisico');
-        $Notificacao = $modelCheck->listaNotificacao($request->Placa);
-        $Notificacao = json_decode($Notificacao);
-        $Veiculos = $controllerUsuario->ListaVeiculosDoUsuario($modelFisico);
-        $Veiculos = json_decode($Veiculos);
-        return dd($Notificacao);
-        return view('Fisico\Notificacao',['Fisico' => $modelFisico, 'Notificacao' => $Notificacao, 'Veiculos' => $Veiculos]);
+
     }
 }
