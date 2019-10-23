30a1b17b4b0e5a14e67c9ba5ede6e29899ecb955
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-23 14:12:24 -0300

    Update modelAnuncio.php

--- app/modelAnuncio.php
+++ app/modelAnuncio.php
@@ -35,8 +35,6 @@ class modelAnuncio extends Model
 
                 $expiracao = date('Y-m-d"', strtotime('+1 month'));
 
-                return dd($auto_anuncio,$request->txtAnuncio, $request->txtDescricao,$request->txtPreco, $atual,$TipoAnuncio[0]->vl,$expiracao);
-
                 DB::table('tb_anuncio')->insert(
                     [
                         'cd_anuncio' => $auto_anuncio,
