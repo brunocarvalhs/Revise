be2feccbd2c4a103735011d7d6c7660059f0dc24
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-23 14:37:06 -0300

    Update alert.blade.php

--- resources/views/componentes/alert.blade.php
+++ resources/views/componentes/alert.blade.php
@@ -1,9 +1,9 @@
 <div>
     <script>
         Swal.fire({
-            title: '{{ $titulo }}',
-            text: '{{ $mensagem }}',
-            type: '{{ $icone  }}',
+            title: {{ $titulo }},
+            text: {{ $mensagem }},
+            type: {{ $icone  }},
             buttons: false,
             timer: 5000,
         });
