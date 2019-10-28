03adb5d6a0d556a67dd34af763b6459aa53ff6bd
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-28 00:17:58 -0300

    Update DetalhesAnuncio.blade.php

--- resources/views/Fisico/DetalhesAnuncio.blade.php
+++ resources/views/Fisico/DetalhesAnuncio.blade.php
@@ -34,7 +34,7 @@
                             </div>
                             <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mt-5">
                                 <h4 class="card-text text-dark" align="center">{{ $Anuncio->Titulo }}</h4>
-                                @if($Anuncio->valor != null)
+                                @if($Anuncio->Valor != null)
                                     <h3 class="card-text text-dark m-5" align="center">R$ {{ $Anuncio->Valor }} </h3>
                                 @endif
                             </div>
