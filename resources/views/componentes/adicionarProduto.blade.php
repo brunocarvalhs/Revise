8f1c4e26bc388019455f45065b058f83f5b3991a
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-27 22:51:09 -0300

    Update adicionarProduto.blade.php

--- resources/views/componentes/adicionarProduto.blade.php
+++ resources/views/componentes/adicionarProduto.blade.php
@@ -53,7 +53,7 @@
                                             <span class="input-group-text">R$</span>
                                         </div>
                                         <input type="text" class="form-control"
-                                            aria-label="Preço" id="txtPreco" name="txtPreco" data-mask="0000000.00">
+                                            aria-label="Preço" id="txtPreco" name="txtPreco" data-mask="##0.00">
                                     </div>
                                 </div>
                             </div>
