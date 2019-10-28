fcb9691c12b4fd2d19525a4fa37f0d9d73e5e1c2
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-10-27 23:13:09 -0300

    Update DetalhesAnuncio.blade.php

--- resources/views/Fisico/DetalhesAnuncio.blade.php
+++ resources/views/Fisico/DetalhesAnuncio.blade.php
@@ -19,10 +19,32 @@
             <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 paddingCards">
                 <div class="card bg-white h-100">
                     <div class="card-body ">
-                        <h4 class="card-text text-dark" align="center">{{ $Anuncio->Titulo }}</h4>
-
-                        <p class="card-text text-dark mt-5" align="justify">{{ $Anuncio->Descricao }}</p>
-                        <h5 class="card-text text-dark m-5" align="center"> Preço : R$ {{ $Anuncio->Valor }} </p>
+                        <div class="row">
+                            <div class="col-6">
+                                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=Primeiro Slide" alt="Primeiro Slide">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Segundo Slide" alt="Segundo Slide">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Terceiro Slide" alt="Terceiro Slide">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <h4 class="card-text text-dark" align="center">{{ $Anuncio->Titulo }}</h4>
+                                <h5 class="card-text text-dark m-5" align="center"> Preço : R$ {{ $Anuncio->Valor }} </h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="card-text text-dark mt-5" align="justify">{{ $Anuncio->Descricao }}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
