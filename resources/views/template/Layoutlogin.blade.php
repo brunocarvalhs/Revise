db64109febb7feb87c7df73ed19e25d924584af7
bruno <bruno.silva.carvalho@outlook.com,br>
2019-11-04 18:29:27 +0000

    Ajuste de login e app

--- resources/views/template/Layoutlogin.blade.php
+++ resources/views/template/Layoutlogin.blade.php
@@ -25,7 +25,7 @@
                     $("#cpfcnpj").unmask();
                 } catch (e) {}
                 var tamanho = $("#cpfcnpj").val().length;
-                if(tamanho <= 11){
+                if(tamanho < 11){
                     $("#cpfcnpj").mask("999.999.999-99");
                 } else {
                     $("#cpfcnpj").mask("99.999.999/9999-99");
