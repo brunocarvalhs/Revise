1e1ece9d1c567fdc2a41070e6e1a4fae8c1b1dab
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-05 16:31:00 -0300

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,9 +2,25 @@
 
 @section('title', __('Not Found'))
 @section('code')
-<div class="padding">
+<style>
+    .container {
+        width: 100vw;
+        height: 100vh;
+        background: #6C7A89;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center
+    }
+    .box {
+        width: 300px;
+        height: 300px;
+        background: #fff;
+    }
+</style>
+<div class="padding container">
     <div class="row">
-        <div class="col">
+        <div class="col box">
             <img src="{{ asset('img/erros/404.png') }}" width="400" height="400">
         </div>
     </div>
