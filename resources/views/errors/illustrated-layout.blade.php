8219f60c4dae0bb77b12aeb0c9629f3c7099ff95
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-05 15:56:56 -0300

    Update illustrated-layout.blade.php

--- resources/views/errors/illustrated-layout.blade.php
+++ resources/views/errors/illustrated-layout.blade.php
@@ -472,7 +472,7 @@
 
                     <a href="{{ app('router')->has('home') ? route('home') : url('/') }}">
                         <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
-                            {{ __('Go Home') }}
+                            {{ __('Voltar') }}
                         </button>
                     </a>
                 </div>
