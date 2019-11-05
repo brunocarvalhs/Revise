1b4db462b75245c1ab5f090a3c4f7ef3bbb7cf8f
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-05 16:43:45 -0300

    Update illustrated-layout.blade.php

--- resources/views/errors/illustrated-layout.blade.php
+++ resources/views/errors/illustrated-layout.blade.php
@@ -474,7 +474,7 @@
     </head>
     <body class="antialiased font-sans container">
         <div class="md:flex min-h-screen">
-            <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
+            <div class="w-full md:w-1/2 bg-white flex items-center justify-center box">
                 <div class="max-w-sm m-8 box">
                     <div class="text-black text-5xl md:text-15xl font-black">
                         @yield('code', __('Oh no'))
