b07952895daae8f5ede3f8b0f28ac1de26bc012b
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-05 15:43:25 -0300

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
 @section('title', __('Not Found'))
-@section('code', '<img src="{{ asset('img/erros/404.png') }}">')
+@section('code', __('<img src="{{ asset('img/erros/404.png') }}">'))
 @section('message', __('Pagina não encontrada'))
