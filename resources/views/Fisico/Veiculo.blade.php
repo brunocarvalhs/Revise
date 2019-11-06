17c40a9a9a93d657795e23df7eec168cea269e0c
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-06 03:02:23 -0300

    Update Veiculo.blade.php

--- resources/views/Fisico/Veiculo.blade.php
+++ resources/views/Fisico/Veiculo.blade.php
@@ -108,16 +108,64 @@
         </div>
         <div class="row">
             <div class="col-12">
-
+                <div class="card shadow p-3 bg-white rounded">
+                    <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputEmail4">Email</label>
+                                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="inputPassword4">Senha</label>
+                                <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Endereço</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="Rua dos Bobos, nº 0">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress2">Endereço 2</label>
+                            <input type="text" class="form-control" id="inputAddress2"
+                                placeholder="Apartamento, hotel, casa, etc.">
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="inputCity">Cidade</label>
+                                <input type="text" class="form-control" id="inputCity">
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="inputEstado">Estado</label>
+                                <select id="inputEstado" class="form-control">
+                                    <option selected>Escolher...</option>
+                                    <option>...</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-md-2">
+                                <label for="inputCEP">CEP</label>
+                                <input type="text" class="form-control" id="inputCEP">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="gridCheck">
+                                <label class="form-check-label" for="gridCheck">
+                                    Clique em mim
+                                </label>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Entrar</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-@endsection
+    @endsection
 
 
 
 
-@section('script')
+    @section('script')
     <script>
         $(document).ready(function () {
             var itemsMainDiv = ('.MultiCarousel');
@@ -227,27 +275,27 @@
         });
     </script>
     <script src="{{asset('js/API.js')}}"></script>
-@endsection
+    @endsection
 
 
-@section('mensagem')
-@if(session()->has('Cadastro'))
+    @section('mensagem')
+    @if(session()->has('Cadastro'))
     @if(session()->get('Cadastro')->Status)
-        @component('componentes.alert')
-            @slot('titulo','SUCESSO')
-            @slot('icone','success')
-            @slot('mensagem')
-                {{ session()->get('Cadastro')->Mensagem }}
-            @endslot
-        @endcomponent
+    @component('componentes.alert')
+    @slot('titulo','SUCESSO')
+    @slot('icone','success')
+    @slot('mensagem')
+    {{ session()->get('Cadastro')->Mensagem }}
+    @endslot
+    @endcomponent
     @else
-        @component('componentes.alert')
-            @slot('titulo','ERRO')
-            @slot('icone','error')
-            @slot('mensagem')
-                {{ session()->get('Cadastro')->Mensagem }}
-            @endslot
-        @endcomponent
+    @component('componentes.alert')
+    @slot('titulo','ERRO')
+    @slot('icone','error')
+    @slot('mensagem')
+    {{ session()->get('Cadastro')->Mensagem }}
+    @endslot
+    @endcomponent
     @endif
-@endif
-@endsection
+    @endif
+    @endsection
