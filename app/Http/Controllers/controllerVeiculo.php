55aa99b1b7b03aa10407e5f5508966171d70dd4a
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-07 01:20:29 -0300

    testes

--- app/Http/Controllers/controllerVeiculo.php
+++ app/Http/Controllers/controllerVeiculo.php
@@ -19,7 +19,7 @@ class controllerVeiculo extends Controller
         $modelFisico = session()->get('Fisico');
         $Veiculos = $controllerUsuario->ListaVeiculosDoUsuario($modelFisico);
         $Veiculos = json_decode($Veiculos);
-        return view('Fisico\Veiculo',['Fisico' => $modelFisico, 'Veiculos' => $Veiculos]);
+        return view('Fisico\Veiculo',['Fisico' => $modelFisico, 'Veiculos' => $Veiculos])->with('status', 'ABC1234');
     }
 
     public function AdicionarVeiculo(Request $request,modelVeiculo $modelVeiculo, $usuario){
