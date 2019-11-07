cc87e306946ee8676a8e4aba0efff0870636a056
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-07 03:14:21 -0300

    Update checklist.blade.php

--- resources/views/componentes/checklist.blade.php
+++ resources/views/componentes/checklist.blade.php
@@ -6,7 +6,7 @@
             <input type="hidden" name="txtTipo" value="Serviço">
             <div class="modal-content">
                 <div class="modal-header text-center">
-                <h4 class="modal-title w-100 font-weight-bold">Checklist ({{ $placa }})</h4>
+                <h4 class="modal-title w-100 font-weight-bold">Checklist<br>({{ $placa }})</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
