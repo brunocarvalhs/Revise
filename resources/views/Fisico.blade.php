4ab30e19d5c68ab2459d50552c584351701b8147
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-07 20:21:11 -0300

    Termos de uso

--- resources/views/Fisico.blade.php
+++ resources/views/Fisico.blade.php
@@ -156,6 +156,15 @@
                     </div>
                 </div>
             </div>
+            <!-- Termos de uso -->
+            <div class="row mt-5">
+                <div class="col-12">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">Aceito os <a href="" data-toggle="modal" data-target="#modalTermoDeUso" >Termos de uso</a></label>
+                    </div>
+                </div>
+            </div>
             <!--Botão-->
             <br><br>
             <div class="row">
@@ -167,6 +176,8 @@
     </div>
     </form>
 </section>
+@component('componentes.termosdeuso')
+@endcomponent
 @endsection
 
 @section('script')
