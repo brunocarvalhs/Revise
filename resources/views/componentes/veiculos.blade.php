56fe6fe16e5d4ce31d105de8c6ecd72b82826bb9
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-08 01:17:43 -0300

    Update veiculos.blade.php

--- resources/views/componentes/veiculos.blade.php
+++ resources/views/componentes/veiculos.blade.php
@@ -1,6 +1,6 @@
 <div class="col-3">
-    <div class="item m-1">
-        <div class="pad15 shadow-sm bg-light rounded">
+    <div>
+        <div>
             <button class="btn btn-dark" data-toggle="modal"
                 data-target="#modelQuilometragem" onclick="quilometragem('{{ $placa }}')">
                 <p class="lead">{{ $placa }}</p>
