dd0cd1a7bacf732d1aae524c319333da5a0c8e85
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-08 03:44:26 -0300

    Update contagem.blade.php

--- resources/views/componentes/contagem.blade.php
+++ resources/views/componentes/contagem.blade.php
@@ -5,31 +5,30 @@
         <small class="text-muted"></small>
     </div>
     <div class="toast-body bg-ligth">
-        @if ($Quilometragem >= 5000)
+        @if ($Quilometragem >= 5000 && $Porcentagem >= 0)
             <div class="form-group">
                 <label for="disabledTextInput">Quilometragem - {{round($Porcentagem,2)}}%</label>
                     @if($Porcentagem >= 90)
                         <progress class="progress is-medium is-danger" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
                     @elseif($Porcentagem >= 50)
                         <progress class="progress is-medium is-warning" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
-                    @elseif($Porcentagem >= 0)
-                        <progress class="progress is-medium" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
                     @else
-
+                        <progress class="progress is-medium" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
                     @endif
             </div>
-        @else
+        @elseif($Porcentagem >= 0)
             <div class="form-group">
                 <label for="disabledTextInput">Tempo - {{round($Porcentagem,2)}}%</label>
                     @if($Porcentagem >= 90)
                         <progress class="progress is-medium is-danger" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
                     @elseif($Porcentagem >= 50)
                         <progress class="progress is-medium is-warning" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
-                    @elseif($Porcentagem >= 0)
-                        <progress class="progress is-medium" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
                     @else
+                            <progress class="progress is-medium" value="{{round($Porcentagem,2)}}" min="0" max="100">{{round($Porcentagem,2)}}</progress>
                     @endif
             </div>
+        @else
+
         @endif
     </div>
 </div>
