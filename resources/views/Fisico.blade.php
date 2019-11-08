c6b9b82a6b62ca9df3191e08578c36b19e6d959e
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-11-08 09:53:02 -0300

    Update Fisico.blade.php

--- resources/views/Fisico.blade.php
+++ resources/views/Fisico.blade.php
@@ -126,8 +126,6 @@
 
                                         </ul>
                                     </p>
-                                    <div class="pt-4"></div>
-                                    <button type="button" class="btn btn-warning w-100">Assine já</button>
                                 </div>
                             </div>
                         </div>
@@ -145,32 +143,12 @@
                                             <li class="planos_padding h6">Visualização dos anúncios postados pelos
                                                 fornecedores
                                             </li>
-
                                         </ul>
                                     </p>
-                                    <div class="pt-4"></div>
-                                    <button type="button" class="btn btn-warning w-100">Assine já</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <!-- Termos de uso -->
-            <div class="row mt-5">
-                <div class="col-12">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                        <label class="form-check-label" for="inlineCheckbox1">Aceito os <a href="" data-toggle="modal" data-target="#modalTermoDeUso" >Termos de uso</a></label>
-                    </div>
-                </div>
-            </div>
-            <!--Botão-->
-            <br><br>
-            <div class="row">
-                <div class="col-0 col-sm-0 col-md-0 col-lg-8 col-xl-8"></div>
-                <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
-                    <button class="btn btn-warning btn-block" id="btnContinua">Cadastrar</button>
                 </div>
             </div>
     </div>
