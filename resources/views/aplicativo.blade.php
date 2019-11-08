c6f5a98e47f99cd1611abb515d33b06c5bb39ec4
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-11-08 15:39:14 -0300

    Update aplicativo.blade.php

--- resources/views/aplicativo.blade.php
+++ resources/views/aplicativo.blade.php
@@ -10,15 +10,16 @@
         <br>
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 align-self-start">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 TituloAplicativoIntroducao">
-                            <h1 class="tituloBranco Titulo espaco">
-                                Revise App
-                            </h1>
-                        </div>
-                        <div class="col-12">
-
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 TituloAplicativoIntroducao">
+                                <h1 class="tituloBranco Titulo espaco">
+                                    Revise App
+                                </h1>
+                            </div>
+                            <div class="col-12">
+                    
+                            </div>
                         </div>
                     </div>
                 </div>
