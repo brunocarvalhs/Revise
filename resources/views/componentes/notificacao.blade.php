520d7dfd9f7546c068974c8bb4ea68fffde07e0b
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-10 16:32:42 -0300

    Update notificacao.blade.php

--- resources/views/componentes/notificacao.blade.php
+++ resources/views/componentes/notificacao.blade.php
@@ -8,19 +8,19 @@
         Opa, Chegou a hora da verificar o {{ $Nome }}!<br><br>
         Que tal já realizar suas pesquisas?Faça isso em Serviços e Produtos.<br>
         <div class="row mt-3">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-3">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3">
                 <a href="{{ route('pesquisa') }}?pesquisa={{ urlencode($Nome) }}&tipo=&preco=">
                     <button class="btn btn-success mt-3">Pesquisar</button>
                 </a>
             </div>
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl">
+            <div class="col-12 col-sm-12 col-md-12 col-lg col-xl">
                 <form action="" method="POST">
                     <button class="btn btn-dark mt-3">Problema resolvido</button>
                     @csrf
                     @method('delete')
                 </form>
             </div>
-            <div class="col-0 col-sm-0 col-md-0 col-lg-0 col-xl-4"></div>
+            <div class="col-0 col-sm-0 col-md-0 col-lg-4 col-xl-4"></div>
         </div>
     </div>
 </div>
