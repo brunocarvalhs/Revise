493abadd76fee924d1e7c71ab99611f2f59aea7f
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-10 16:27:09 -0300

    Update notificacao.blade.php

--- resources/views/componentes/notificacao.blade.php
+++ resources/views/componentes/notificacao.blade.php
@@ -8,7 +8,7 @@
         Opa, Chegou a hora da verificar o {{ $Nome }}!<br><br>
         Que tal já realizar suas pesquisas?Faça isso em Serviços e Produtos.<br>
         <div class="row mt-3">
-            <div class="col">
+            <div class="col-2">
                 <a href="{{ route('pesquisa') }}?pesquisa={{ urlencode($Nome) }}&tipo=&preco=">
                     <button class="btn btn-success mt-3">Pesquisar</button>
                 </a>
@@ -20,6 +20,7 @@
                     @method('delete')
                 </form>
             </div>
+            <div class="col-5"></div>
         </div>
     </div>
 </div>
