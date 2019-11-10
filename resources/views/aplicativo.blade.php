28a55698b7a165321d64e342353f06054cf6c661
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-11-10 00:48:27 -0300

    Update aplicativo.blade.php

--- resources/views/aplicativo.blade.php
+++ resources/views/aplicativo.blade.php
@@ -20,39 +20,36 @@
         </div>
     </div>
 </section>
-<section class="bg-light" id="sobre">
-    <div class="container">
+<!-- particles.js container -->
+
+<div id="particles-js"></div>
+<div class="container">
         <div class="row">
-            <div class="col">
-                <div class="col-12 col-sm-12 col-md-auto col-lg-auto col-xl-auto text-dark">
-                    <br><br>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-light pt-5">
                     <h1>O que é o Revise App?</h1>
-                    <hr>
                     <br>
                 </div>
-            </div>
         </div>
+</div>
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5">
-
+            <div class="col-1 col-sm-6 col-md-6 col-lg-6 col-xl-5">
                 <img id="imgRequisito" src="{{ asset('img/appimg.png') }}" class="AppSmart pl-5">
             </div>
-            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <p class="text-dark h5 mt-5">
+            <div class="col-11 col-sm-6 col-md-6 col-lg-6 col-xl-5 pt-4">
+                <p class="text-light h5 mt-5">
                     Acesse as funcionalidades do sistema em seu smartphone ou
                     tablet a qualquer momento e ainda confira sua quilometragem com a tecnologia
                     do sensor Obd2, caso seja usuário Gold
                 </p>
             </div>
         </div>
-    </div>
-    </div>
-    <br><br>
+</div>
+<br><br>
 </section>
 <section class="backgroundWhite" id="requisitos">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 tituloPreto">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 tituloPreto mt-5">
                 <h1 class="text-dark">Requisitos do aplicativo</h1>
                 <hr>
                 <br>
@@ -249,6 +246,30 @@
         </form>
     </div>
 </section>
+<!-- particles.js lib - https://github.com/VincentGarreau/particles.js -->
+<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<!-- stats.js lib -->
+<style>
+    /* ---- reset ---- */
+    body {
+        margin: 0;
+    }
+
+    /* ---- particles.js container ---- */
+    #particles-js {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: #26282b;
+        background-image: url("");
+        background-repeat: no-repeat;
+        background-size:inherit;
+        background-position: 50% 50%;
+    }    
+</style>
+<script>
+    particlesJS("particles-js", { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true }); var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function () { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+</script>
 @endsection
 
 @section('script')
