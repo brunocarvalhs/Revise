678a252291c941a3d75ba108afd1e7db18e4fa9b
Feh48 <fernandapereirasantosdasilva@gmail.com>
2019-11-10 00:53:00 -0300

    Update aplicativo.blade.php

--- resources/views/aplicativo.blade.php
+++ resources/views/aplicativo.blade.php
@@ -44,13 +44,12 @@
             </div>
         </div>
 </div>
-<br><br>
 </section>
 <section class="backgroundWhite" id="requisitos">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 tituloPreto mt-5">
-                <h1 class="text-dark">Requisitos do aplicativo</h1>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 tituloPreto ">
+                <h1 class="text-dark mt-3">Requisitos do aplicativo</h1>
                 <hr>
                 <br>
             </div>
