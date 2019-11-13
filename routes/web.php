58d9debf72b58f4d261b7d0a27f2ac8b909650d8
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-13 20:29:06 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::get('/voltar', function(){
 })->name('iwillbeback');//Referencias do Pedrão
 
 Route::post('/Suporte', function(){
-    return back();
+    return redirect('/');
 });
 
 //Rotas Paticulares
