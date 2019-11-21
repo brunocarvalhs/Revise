f004c4bf7820b912692e4edcf9e947555035b239
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-21 14:35:06 -0300

    Update Inicio.blade.php

--- resources/views/Juridico/Inicio.blade.php
+++ resources/views/Juridico/Inicio.blade.php
@@ -20,7 +20,12 @@
 <div class="row">
     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <div class="row">
-            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 p-3">
+            <div class="col-12 col-sm-12 col-md-0 col-lg-0 col-xl-0 p-3">
+                <div class="col-12 card hadow-sm p-3 mb-5 bg-white rounded">
+                    <canvas id="Anuncios" width="400" height="400"></canvas>
+                </div>
+            </div>
+            <div class="col-0 col-sm-0 col-md-6 col-lg-6 col-xl-6 p-3">
                 <div class="col-12 card hadow-sm p-3 mb-5 bg-white rounded">
                     <canvas id="AnunciosHorizontal" width="600" height="200"></canvas>
                 </div>
@@ -41,6 +46,7 @@
 
 
 @section('script')
+    @include('componentes.graficoAnuncios')
     @include('componentes.graficoIndice')
     @include('componentes.graficoHorizantal')
 @endsection
