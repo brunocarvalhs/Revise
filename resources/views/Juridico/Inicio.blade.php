f252270e8c70a925194d7179bae5b03abb2ab64d
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-21 14:08:59 -0300

    Update Inicio.blade.php

--- resources/views/Juridico/Inicio.blade.php
+++ resources/views/Juridico/Inicio.blade.php
@@ -18,7 +18,7 @@
 
 @section('sistema_juridico')
 <div class="row p-3">
-    <div class="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
+    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <div class="row">
             <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 p-3">
                 <div class="col-12 card hadow-sm p-3 mb-5 bg-white rounded">
@@ -38,11 +38,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 p-3">
-        <ul class="list-group">
-            <li class="list-group-item disabled">Cras justo odio</li>
-        </ul>
     </div>
 </div>
 
