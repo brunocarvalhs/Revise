68947f49e17a56c6feff97246cba6d4b0689bc73
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-23 14:56:35 -0300

    Update PesquisaAnuncio.blade.php

--- resources/views/componentes/PesquisaAnuncio.blade.php
+++ resources/views/componentes/PesquisaAnuncio.blade.php
@@ -5,7 +5,9 @@
                 <h6 class="card-subtitle">{{ $tipo }}</h6>
                 <h4 class="card-title">{{ $titulo }}</h4>
                 <h6 class="card-title">Local: {{ $cidade }} - {{ $uf }}</h6>
-                <h5>{{ $preco }}</h5>
+                @if($preco != null)
+                    <h5>R$ {{ $preco }}</h5>
+                @endif
             <a class="btn btn-outline-warning  w-100 col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4" href="{{ url('/Home/Anuncios') }}/{{ $id }}" role="button"> Mais Detalhes +</a>
             </div>
         </div>
