fa149951a3b766598123036723d3f4c2edb5126d
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-24 16:22:46 -0300

    Update graficoAnuncios.blade.php

--- resources/views/componentes/graficoAnuncios.blade.php
+++ resources/views/componentes/graficoAnuncios.blade.php
@@ -2,38 +2,40 @@
 
 <div>
     <script>
-        var anuncio = "{{ route('an') }}"; //Rota de coleta de dados do banco
-        var Anuncio = new Array();
-        var Acessos = new Array();
-        $(document).ready(function () {
-            $.get(anuncio, function (response) {
-                response.forEach(function (data) {
-                    Anuncio.push(data.Anuncio);
-                    Acessos.push(data.Visualizacao);
-                });
-                var ctx = document.getElementById("Anuncios").getContext('2d'); // Nome do ID onde será implantodo o grafico
-                var myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: Anuncio,
-                        datasets: [{
-                            label: 'Popularidade do Anúncio',
-                            data: Acessos,
-                            borderWidth: 1,
-                            backgroundColor: 'rgba(255, 193, 7, 0.9)'//Amarelo
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
+        setInterval({
+            var anuncio = "{{ route('an') }}"; //Rota de coleta de dados do banco
+            var Anuncio = new Array();
+            var Acessos = new Array();
+            $(document).ready(function () {
+                $.get(anuncio, function (response) {
+                    response.forEach(function (data) {
+                        Anuncio.push(data.Anuncio);
+                        Acessos.push(data.Visualizacao);
+                    });
+                    var ctx = document.getElementById("Anuncios").getContext('2d'); // Nome do ID onde será implantodo o grafico
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: Anuncio,
+                            datasets: [{
+                                label: 'Popularidade do Anúncio',
+                                data: Acessos,
+                                borderWidth: 1,
+                                backgroundColor: 'rgba(255, 193, 7, 0.9)'//Amarelo
                             }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
                         }
-                    }
+                    });
                 });
             });
-        });
+        }, 1000);
     </script>
 </div>
