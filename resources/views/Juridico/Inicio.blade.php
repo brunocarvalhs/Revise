68bc88fec6de9421c49af3b316d165efd8bd405a
Bruno Silva Carvalho <bruno.silva.carvalho@outlook.com.br>
2019-11-26 11:24:10 -0300

    Update Inicio.blade.php

--- resources/views/Juridico/Inicio.blade.php
+++ resources/views/Juridico/Inicio.blade.php
@@ -23,7 +23,7 @@
             <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 p-3">
                 <div class="col-12 card hadow-sm p-3 mb-5 bg-white rounded">
                     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <h5>Indice de acesso por anucios</h5>
+                        <h5>Índice de acesso por anúncio</h5>
                     </div>
                     <canvas id="Anuncios" width="600" height="200"></canvas>
                 </div>
@@ -31,7 +31,7 @@
             <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 p-3">
                 <div class="col-12 card hadow-sm p-3 mb-5 bg-white rounded">
                     <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <h5>Indice de acesso por categorias</h5>
+                        <h5>Índice de acesso por categoria</h5>
                     </div>
                     <canvas id="Indice" width="400" height="400"></canvas>
                 </div>
